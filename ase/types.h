f804ef148557e83411d61bf3634819a83d10c3c2
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2004-12-18 09:58:49 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@3 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/types.h
+++ ase/types.h
@@ -1,19 +1,19 @@
 /*
- * $Id: types.h,v 1.1 2004-12-18 09:16:31 bacon Exp $
+ * $Id: types.h,v 1.2 2004-12-18 09:58:49 bacon Exp $
  */
 
-#ifndef _RB_TYPES_H_
-#define _RB_TYPES_H_
+#ifndef _XP_TYPES_H_
+#define _XP_TYPES_H_
 
 #include <rb/itypes.h>
 
-typedef rb_int8_t   rb_mchar_t;
-typedef rb_int32_t  rb_mcint_t;
+typedef xp_int8_t   xp_mchar_t;
+typedef xp_int32_t  xp_mcint_t;
 
-typedef rb_int16_t  rb_wchar_t;
-typedef rb_int32_t  rb_wcint_t;
+typedef xp_int16_t  xp_wchar_t;
+typedef xp_int32_t  xp_wcint_t;
 
-typedef rb_mchar_t  rb_char_t;
-typedef rb_mcint_t  rb_cint_t;
+typedef xp_mchar_t  xp_char_t;
+typedef xp_mcint_t  xp_cint_t;
 
 #endif
