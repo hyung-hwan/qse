1027516536ae2cbcdf264e718e8e54b2a196d505
hyung-hwan <hyunghwan.chung@gmail.com>
2004-12-18 09:16:31 +0000

    *** empty log message ***

---
+++ ase/types.h
@@ -0,0 +1,19 @@
+/*
+ * $Id: types.h,v 1.1 2004-12-18 09:16:31 bacon Exp $
+ */
+
+#ifndef _RB_TYPES_H_
+#define _RB_TYPES_H_
+
+#include <rb/itypes.h>
+
+typedef rb_int8_t   rb_mchar_t;
+typedef rb_int32_t  rb_mcint_t;
+
+typedef rb_int16_t  rb_wchar_t;
+typedef rb_int32_t  rb_wcint_t;
+
+typedef rb_mchar_t  rb_char_t;
+typedef rb_mcint_t  rb_cint_t;
+
+#endif
