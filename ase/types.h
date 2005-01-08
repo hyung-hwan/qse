24f52b3e55a1928717846b5003dd0d3c37b06189
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-01-08 13:40:14 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@10 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/types.h
+++ ase/types.h
@@ -1,17 +1,16 @@
 /*
- * $Id: types.h,v 1.7 2005-01-08 12:25:59 bacon Exp $
+ * $Id: types.h,v 1.8 2005-01-08 13:40:14 bacon Exp $
  */
 
 #ifndef _XP_TYPES_H_
 #define _XP_TYPES_H_
 
-#ifdef _WIN32
-#else
 #include <xp/itypes.h>
-#endif
 
 typedef xp_uint8_t  xp_byte_t;
 
+#ifdef XP_HAVE_WCHAR_T
+// TODO: make it configurable from outside
 /*
 #define XP_CHAR_IS_MCHAR
 typedef xp_mchar_t  xp_char_t;
@@ -21,5 +20,11 @@ typedef xp_mcint_t  xp_cint_t;
 #define XP_CHAR_IS_WCHAR
 typedef xp_wchar_t  xp_char_t;
 typedef xp_wcint_t  xp_cint_t;
+
+#else
+#define XP_CHAR_IS_MCHAR
+typedef xp_mchar_t  xp_char_t;
+typedef xp_mcint_t  xp_cint_t;
+#endif
 
 #endif
