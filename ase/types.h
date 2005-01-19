0d3089f9aec83b0dae85194d92a29f11e7f7420d
hyung-hwan <hyunghwan.chung@gmail.com>
2005-01-19 11:58:26 +0000

    *** empty log message ***

--- ase/types.h
+++ ase/types.h
@@ -1,10 +1,13 @@
 /*
- * $Id: types.h,v 1.8 2005-01-08 13:40:14 bacon Exp $
+ * $Id: types.h,v 1.9 2005-01-19 11:58:26 bacon Exp $
  */
 
 #ifndef _XP_TYPES_H_
 #define _XP_TYPES_H_
 
+#ifndef _WIN32
+#include <xp/config.h>
+#endif
 #include <xp/itypes.h>
 
 typedef xp_uint8_t  xp_byte_t;
