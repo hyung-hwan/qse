62de5f25787085d6ca2171c0da2ae42f20d8e017
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-05-08 15:54:44 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@110 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ ase/stx/hash.h
@@ -0,0 +1,16 @@
+/*
+ * $Id: hash.h,v 1.1 2005-05-08 15:54:44 bacon Exp $
+ */
+
+#ifndef _XP_STX_HASH_H_
+#define _XP_STX_HASH_H_
+
+#ifdef __cplusplus
+extern "C"
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
