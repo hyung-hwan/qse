d98a87596f5aa0bbcdbfe8ae18f12540644a49a0
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-05-10 06:02:19 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@112 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/stx/hash.h
+++ ase/stx/hash.h
@@ -1,13 +1,24 @@
 /*
- * $Id: hash.h,v 1.1 2005-05-08 15:54:44 bacon Exp $
+ * $Id: hash.h,v 1.2 2005-05-10 06:02:19 bacon Exp $
  */
 
 #ifndef _XP_STX_HASH_H_
 #define _XP_STX_HASH_H_
 
+#include <xp/stx/stx.h>
+
 #ifdef __cplusplus
 extern "C"
 #endif
+
+xp_stx_word_t xp_stx_new_link (
+	xp_stx_t* stx, xp_stx_word_t key, xp_stx_word_t value);
+xp_stx_word_t xp_stx_hash_lookup (
+	xp_stx_t* stx, xp_stx_word_t table,
+	xp_stx_word_t hash, xp_stx_word_t key);
+void xp_stx_hash_insert (
+	xp_stx_t* stx, xp_stx_word_t table,
+	xp_stx_word_t hash, xp_stx_word_t key, xp_stx_word_t value);
 
 #ifdef __cplusplus
 }
