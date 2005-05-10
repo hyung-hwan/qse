d8e18919da017fa898354fe4eb958e267be1be06
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-05-10 12:00:43 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@115 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/stx/object.h
+++ ase/stx/object.h
@@ -1,5 +1,5 @@
 /*
- * $Id: object.h,v 1.5 2005-05-10 08:21:10 bacon Exp $
+ * $Id: object.h,v 1.6 2005-05-10 12:00:43 bacon Exp $
  */
 
 #ifndef _XP_STX_OBJECT_H_
@@ -21,6 +21,10 @@ xp_stx_word_t xp_stx_alloc_string_object (
 	xp_stx_t* stx, const xp_stx_char_t* str);
 
 xp_stx_word_t xp_stx_hash_string_object (xp_stx_t* stx, xp_stx_word_t idx);
+
+xp_stx_word_t xp_stx_new_string_object (
+	xp_stx_t* stx, const xp_stx_char_t* name, xp_stx_word_t class);
+
 
 #ifdef __cplusplus
 }
