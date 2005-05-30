3dedd5d0e44902fc08932179627beaf0eab45a39
hyung-hwan <hyunghwan.chung@gmail.com>
2005-05-30 15:55:06 +0000

    *** empty log message ***

--- ase/stx/parser.c
+++ ase/stx/parser.c
@@ -1,8 +1,9 @@
 /*
- * $Id: parser.c,v 1.7 2005-05-30 15:24:12 bacon Exp $
+ * $Id: parser.c,v 1.8 2005-05-30 15:55:06 bacon Exp $
  */
 
 #include <xp/stx/parser.h>
+#include <xp/stx/token.h>
 #include <xp/stx/misc.h>
 
 xp_stx_parser_t* xp_stx_parser_open (xp_stx_parser_t* parser)
@@ -31,8 +32,28 @@ void xp_stx_parser_close (xp_stx_parser_t* parser)
 	if (parser->__malloced) xp_stx_free (parser);
 }
 
-int xp_stx_parser_parse (xp_stx_parser_t* parser)
+/* set input stream */
+/*
+int xp_stx_parser_set_ios (xp_stx_parser_t* parser, input_func)
 {
+}
+
+int xp_stx_parser_parse_filein (xp_stx_parser_t* parser)
+{
+	xp_stx_token_t* token;
+
+	token = xp_stx_lexer_consume (&parser->lexer);
+	if (token == XP_NULL) return -1;
+
+	if (token->type == XP_STX_TOKEN_EXCLM) 
+}
+*/
+
+int xp_stx_parser_parse_method (
+	xp_stx_parser_t* parser, xp_stx_word_t class, xp_stx_char_t* text)
+{
+	xp_stx_lexer_reset (&parser->lexer, text);
+
 	parser->token = xp_stx_lexer_consume (&parser->lexer);
 	if (parser->token == XP_NULL) {
 		/*parser->error_code = xxx;*/
@@ -41,5 +62,3 @@ int xp_stx_parser_parse (xp_stx_parser_t* parser)
 
 	return 0;
 }
-
-
