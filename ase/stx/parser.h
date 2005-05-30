4ff42a06f00721e0f2125b1a347240f75fac40b3
hyung-hwan <hyunghwan.chung@gmail.com>
2005-05-30 15:27:31 +0000

    *** empty log message ***

--- ase/stx/parser.h
+++ ase/stx/parser.h
@@ -1,15 +1,19 @@
 /*
- * $Id: parser.h,v 1.4 2005-05-30 07:38:25 bacon Exp $
+ * $Id: parser.h,v 1.5 2005-05-30 15:24:12 bacon Exp $
  */
 
 #ifndef _XP_STX_PARSER_H_
 #define _XP_STX_PARSER_H_
 
 #include <xp/stx/stx.h>
-#include <xp/stx/scanner.h>
+#include <xp/stx/lexer.h>
+#include <xp/stx/token.h>
 
 struct xp_stx_parser_t
 {
+	xp_stx_lexer_t lexer;
+	xp_stx_token_t* token;
+	int error_code;
 	xp_bool_t __malloced;
 };
 
