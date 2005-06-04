f12dca4a94e89fb5d47f5dd25350fd9b3de906ce
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-06-04 07:01:57 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@177 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/stx/parser.h
+++ ase/stx/parser.h
@@ -1,17 +1,23 @@
 /*
- * $Id: parser.h,v 1.7 2005-06-02 16:14:58 bacon Exp $
+ * $Id: parser.h,v 1.8 2005-06-04 07:01:57 bacon Exp $
  */
 
 #ifndef _XP_STX_PARSER_H_
 #define _XP_STX_PARSER_H_
 
 #include <xp/stx/stx.h>
-#include <xp/stx/lexer.h>
+#include <xp/stx/token.h>
 
 struct xp_stx_parser_t
 {
-	xp_stx_lexer_t lexer;
 	int error_code;
+	xp_stx_token_t token;
+
+	/* lexer data */
+	const xp_char_t* text;
+	const xp_cint_t  curc;
+	const xp_char_t* curp;
+
 	xp_bool_t __malloced;
 };
 
