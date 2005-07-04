0c6b251d78e1d67c9dc6726499761874c11594af
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-07-04 16:23:13 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@226 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/stx/class.h
+++ ase/stx/class.h
@@ -1,5 +1,5 @@
 /*
- * $Id: class.h,v 1.8 2005-07-03 16:37:01 bacon Exp $
+ * $Id: class.h,v 1.9 2005-07-04 16:23:13 bacon Exp $
  */
 
 #ifndef _XP_STX_CLASS_H_
@@ -25,10 +25,15 @@
 #define XP_STX_METACLASS_SUBCLASSES     3
 #define XP_STX_METACLASS_INSTANCE_CLASS 4
 
+#define XP_STX_SPEC_NOT_INDEXABLE  0
+#define XP_STX_SPEC_WORD_INDEXABLE 1
+#define XP_STX_SPEC_BYTE_INDEXABLE 2
+#define XP_STX_SPEC_CHAR_INDEXABLE 3
+
 struct xp_stx_class_t
 {
 	xp_stx_objhdr_t header;
-	xp_word_t spec; /* indexable: 1, nfields: the rest */
+	xp_word_t spec; /* indexable: 2, nfields: the rest */
 	xp_word_t methods;
 	xp_word_t superclass;
 	xp_word_t subclasses;
