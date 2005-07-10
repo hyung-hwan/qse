c862045fb7322859f44edd9b05dde577c2aa9eba
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2005-07-10 03:16:40 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@242 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/stx/bytecode.h
+++ ase/stx/bytecode.h
@@ -1,5 +1,5 @@
 /*
- * $Id: bytecode.h,v 1.5 2005-07-08 11:32:50 bacon Exp $
+ * $Id: bytecode.h,v 1.6 2005-07-10 03:16:40 bacon Exp $
  */
 
 #ifndef _XP_STX_BYTECODE_H_
@@ -7,8 +7,34 @@
 
 #include <xp/stx/stx.h>
 
-#define PUSH_RECEIVER_VARIABLE     0x00
-#define PUSH_RECEIVER_VARIABLE_BIG 0x10
+#define PUSH_RECEIVER_VARIABLE            0x00
+#define PUSH_TEMPORARY_LOCATION           0x10
+#define PUSH_LITERAL_CONSTANT             0x20
+#define PUSH_LITERAL_VARIABLE             0x30
+#define STORE_RECEIVER_VARIABLE           0x40
+#define STORE_TEMPORARY_LOCATION          0x50
+
+#define PUSH_RECEIVER_VARIABLE_EXTENDED   0x60
+#define PUSH_TEMPORARY_LOCATION_EXTENDED  0x61
+#define PUSH_LITERAL_CONSTANT_EXTENDED    0x62
+#define PUSH_LITERAL_VARIABLE_EXTENDED    0x63
+#define STORE_RECEIVER_VARIABLE_EXTENDED  0x64
+#define STORE_TEMPORARY_LOCATION_EXTENDED 0x65
+#define STORE_POP_STACK_TOP               0x68
+#define DUPLICATE_POP_STACK_TOP           0x69
+#define PUSH_ACTIVE_CONTEXT               0x6A
+
+#define SEND_SELECTOR_TO_SELF             0x70
+#define SEND_SELECTOR_TO_SUPER            0x71
+#define SEND_SELECTOR_TO_SELF_EXTENDED    0x72
+#define SEND_SELECTOR_TO_SUPER_EXTENDED   0x73
+
+#define RETURN_RECEIVER                   0x78
+#define RETURN_TRUE                       0x79
+#define RETURN_FALSE                      0x7A
+#define RETURN_NIL                        0x7B
+#define RETURN_FROM_MESSAGE               0x7C
+#define RETURN_FROM_BLOCK                 0x7D
 
 #define PUSH_VARIABLE            0x0
 #define PUSH_TEMPORARY           0x1
@@ -33,9 +59,6 @@
 #define RETURN_FALSE         4
 #define RETURN_FROM_MESSAGE  5
 #define RETURN_FROM_BLOCK    6
-
-#define PUSH_VARIABLE(i)    ((i <= 0x0F)?  (i): 
-#define PUSH_VARIABLE(i)     i
 
 #ifdef __cplusplus
 extern "C" {
