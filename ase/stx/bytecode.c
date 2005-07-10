b795ce8408894764661d520bb400e8cbe8ed0243
hyung-hwan <hyunghwan.chung@gmail.com>
2005-07-10 09:21:46 +0000

    *** empty log message ***

--- ase/stx/bytecode.c
+++ ase/stx/bytecode.c
@@ -1,5 +1,5 @@
 /*
- * $Id: bytecode.c,v 1.4 2005-07-08 11:32:50 bacon Exp $
+ * $Id: bytecode.c,v 1.5 2005-07-10 09:21:46 bacon Exp $
  */
 #include <xp/stx/bytecode.h>
 #include <xp/stx/class.h>
@@ -33,33 +33,21 @@ static void __decode1 (xp_stx_t* stx, xp_word_t idx, void* data)
 	__decode2 (stx, data, method_obj);
 }
 
-static const xp_char_t* opcode_names[] =
-{
-	XP_TEXT("PUSH_VARIABLE"),
-	XP_TEXT("PUSH_TEMPORARY"),
-	XP_TEXT("PUSH_LITERAL_CONSTANT"),
-	XP_TEXT("PUSH_LITERAL_VARIABLE"),
-	XP_TEXT("STORE_VARIABLE"),
-	XP_TEXT("STORE_TEMPORARY"),
-	XP_TEXT("SEND"),
-	XP_TEXT("JUMP"),
-	XP_TEXT("DO_SPECIAL"),
-	XP_TEXT("DO_PRIMITIVE"),
-	XP_TEXT("PUSH_VARIABLE_EXTENDED"),
-	XP_TEXT("PUSH_TEMPORARY_EXTENDED"),
-	XP_TEXT("STORE_VARIABLE_EXTENDED"),
-	XP_TEXT("STORE_TEMPORARY_EXTENDED"),
-	XP_TEXT("DO_SPECIAL_EXTENDED"),
-	XP_TEXT("DO_PRIMITIVE_EXTENDED")
-};
-
 static int __decode2 (xp_stx_t* stx, 
 	xp_stx_class_t* class_obj, xp_stx_method_t* method_obj)
 {
 	xp_stx_byte_object_t* bytecodes;
 	xp_word_t bytecode_size, pc = 0;
-	xp_byte_t code;
-	int opcode, operand;
+	int code, next;
+
+	static const xp_char_t* stack_opcode_names[] = {
+		XP_TEXT("push_receiver_variable"),	
+		XP_TEXT("push_temporary_location"),	
+		XP_TEXT("push_literal_constant"),	
+		XP_TEXT("push_literal_variable"),	
+		XP_TEXT("store_receiver_variable"),	
+		XP_TEXT("store_temporary_location")
+	};
 
 	bytecodes = XP_STX_BYTE_OBJECT(stx, method_obj->bytecodes);
 	bytecode_size = XP_STX_SIZE(stx, method_obj->bytecodes);
@@ -67,23 +55,36 @@ static int __decode2 (xp_stx_t* stx,
 	while (pc < bytecode_size) {
 		code = bytecodes->data[pc++];
 
-		opcode = (code & 0xF0) >> 4;
-		operand = code & 0x0F;
-		if (opcode > 0x9) {
-			if (pc >= bytecode_size) {
-				/* TODO: */	
-				xp_printf (XP_TEXT("error in bytecodes\n"));
-				return -1;
-			}
-			code = bytecodes->data[pc++];
-			operand |= (code << 4);
+		if (code >= 0x00 && code <= 0x5F) {
+			/* stack */
+			xp_printf (XP_TEXT("%s %d\n"), 
+				stack_opcode_names[code >> 4], code & 0x0F);
 		}
-
-		xp_printf (XP_TEXT("%s(0x%x), operand = %d\n"), 
-			opcode_names[opcode], opcode, operand);
+		else if (code >= 0x60 && code <= 0x65) {
+			/* stack extended */
+			next = bytecodes->data[pc++];
+			xp_printf (XP_TEXT("%s %d\n"), 
+				stack_opcode_names[code & 0x0F], next);
+		}
+		else if (code >= 0x70 && code <=  0x73 ) {
+			/* send */
+		}
+		else if (code >= 0x78 && code <= 0x7D) {
+			// return
+		}
+		else if (code >= 0x80 && code <= 0x8F) {
+			// jump
+		}
+		else if (code >= 0xF0 && code <= 0xFF) {
+			// primitive
+			next = bytecodes->data[pc++];
+			xp_printf (XP_TEXT("do_primitive %d\n"), ((code & 0x0F) << 8) | next);
+	
+		}
+		else {
+			xp_printf (XP_TEXT("unknown byte code 0x%x\n"), code);
+		}
 	}
-
 
         return 0;
 }
-
