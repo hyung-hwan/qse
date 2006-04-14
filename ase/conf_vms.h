073ef3290882421658885862f97ed717ec080013
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2006-04-14 14:17:09 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@515 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/conf_vms.h
+++ ase/conf_vms.h
@@ -3,59 +3,60 @@
 /* both vax and alpha are in the little endian. */
 #define XP_ENDIAN_LITTLE 
 
-#define SIZEOF_CHAR 1
-#define SIZEOF_SHORT 2
-#define SIZEOF_INT 4
-#define SIZEOF_LONG 4
+#define XP_SIZEOF_CHAR 1
+#define XP_SIZEOF_SHORT 2
+#define XP_SIZEOF_INT 4
+#define XP_SIZEOF_LONG 4
 #if defined(vax) || defined(__vax)
-	#define SIZEOF_LONG_LONG 0
+	#define XP_SIZEOF_LONG_LONG 0
 #elif defined(alpha) || defined(__alpha)
-	#define SIZEOF_LONG_LONG 8
+	#define XP_SIZEOF_LONG_LONG 8
 #elif defined(itanium) || defined(__itanium)
-	#define SIZEOF_LONG_LONG 8
+	#define XP_SIZEOF_LONG_LONG 8
 #else
-	#define SIZEOF_LONG_LONG 0
+	#define XP_SIZEOF_LONG_LONG 0
 #endif
 
-#define SIZEOF___INT8 1
-#define SIZEOF___INT16 2
-#define SIZEOF___INT32 4
+#define XP_SIZEOF___INT8 1
+#define XP_SIZEOF___INT16 2
+#define XP_SIZEOF___INT32 4
 #if defined(vax) || defined(__vax)
-	#define SIZEOF___INT64 0
+	#define XP_SIZEOF___INT64 0
 #elif defined(alpha) || defined(__alpha)
-	#define SIZEOF___INT64 8
+	#define XP_SIZEOF___INT64 8
 #elif defined(itanium) || defined(__itanium)
-	#define SIZEOF___INT64 8
+	#define XP_SIZEOF___INT64 8
 #else
-	#define SIZEOF___INT64 0
+	#define XP_SIZEOF___INT64 0
 #endif
-#define SIZEOF___INT128 0
+#define XP_SIZEOF___INT96 0
+#define XP_SIZEOF___INT128 0
 
 #if defined(vax) || defined(__vax)
-	#define SIZEOF_VOID_P 4
+	#define XP_SIZEOF_VOID_P 4
 #elif defined(alpha) || defined(__alpha)
 	/*#pragma pointer_size 32
-	#define SIZEOF_VOID_P 4*/
+	#define XP_SIZEOF_VOID_P 4*/
 	#pragma pointer_size 64
-	#define SIZEOF_VOID_P 8
+	#define XP_SIZEOF_VOID_P 8
 #elif defined(itanium) || defined(__itanium)
 	/*#pragma pointer_size 32
-	#define SIZEOF_VOID_P 4*/
+	#define XP_SIZEOF_VOID_P 4*/
 	#pragma pointer_size 64
-	#define SIZEOF_VOID_P 8
+	#define XP_SIZEOF_VOID_P 8
 #else
-	#define SIZEOF_VOID_P 0
+	#define XP_SIZEOF_VOID_P 0
 #endif
 
-#define SIZEOF_FLOAT 4
-#define SIZEOF_DOUBLE 8
+#define XP_SIZEOF_FLOAT 4
+#define XP_SIZEOF_DOUBLE 8
 
 #if defined(vax) || defined(__vax)
-	#define SIZEOF_LONG_DOUBLE 8
+	#define XP_SIZEOF_LONG_DOUBLE 8
 #elif defined(alpha) || defined(__alpha)
-	#define SIZEOF_LONG_DOUBLE 16
+	#define XP_SIZEOF_LONG_DOUBLE 16
 #elif defined(itanium) || defined(__itanium)
-	#define SIZEOF_LONG_DOUBLE 16
+	#define XP_SIZEOF_LONG_DOUBLE 16
 #else
-	#define SIZEOF_LONG_DOUBLE 0
+	#define XP_SIZEOF_LONG_DOUBLE 0
 #endif
