d0c9c8e09936372ab152bd6b28770fbdd1c00bdb
hyung-hwan <hyunghwan.chung@gmail.com>
2006-04-14 14:17:09 +0000

    *** empty log message ***

--- ase/types.h
+++ ase/types.h
@@ -1,5 +1,5 @@
 /*
- * $Id: types.h,v 1.42 2006-04-05 15:20:47 bacon Exp $
+ * $Id: types.h,v 1.43 2006-04-14 14:17:09 bacon Exp $
  */
 
 #ifndef _XP_TYPES_H_
@@ -94,10 +94,10 @@ typedef int xp_tri_t;
 #define xp_dead   -1
 
 /* integer that can hold a pointer */
-#if SIZEOF_VOID_P == SIZEOF_LONG_LONG
+#if XP_SIZEOF_VOID_P == XP_SIZEOF_LONG_LONG
 	typedef long long xp_int_t;
 	typedef unsigned long long xp_uint_t;
-#elif SIZEOF_VOID_P == SIZEOF_LONG
+#elif XP_SIZEOF_VOID_P == XP_SIZEOF_LONG
 	typedef long xp_int_t;
 	typedef unsigned long xp_uint_t;
 #else
@@ -106,10 +106,10 @@ typedef int xp_tri_t;
 #endif
 
 /* the largest integer supported by the system */
-#if SIZEOF_LONG_LONG != 0
+#if XP_SIZEOF_LONG_LONG != 0
 	typedef long long xp_long_t;
 	typedef unsigned long long xp_ulong_t;
-#elif SIZEOF___INT64 != 0
+#elif XP_SIZEOF___INT64 != 0
 	typedef __int64 xp_long_t;
 	typedef unsigned __int64 xp_ulong_t;
 #else
@@ -118,71 +118,71 @@ typedef int xp_tri_t;
 #endif
 
 /* integers of specific size */
-#if SIZEOF_CHAR == 1
+#if XP_SIZEOF_CHAR == 1
 	typedef char xp_int8_t;
 	typedef unsigned char xp_uint8_t;
-#elif SIZEOF___INT8 == 1
+#elif XP_SIZEOF___INT8 == 1
 	typedef __int8 xp_int8_t;
 	typedef unsigned __int8 xp_uint8_t;
 #endif
 
-#if SIZEOF_SHORT == 2
+#if XP_SIZEOF_SHORT == 2
 	typedef short xp_int16_t;
 	typedef unsigned short xp_uint16_t;
-#elif SIZEOF___INT16 == 2
+#elif XP_SIZEOF___INT16 == 2
 	typedef __int16 xp_int16_t;
 	typedef unsigned __int16 xp_uint16_t;
 #endif
 
-#if SIZEOF_INT == 4
+#if XP_SIZEOF_INT == 4
 	typedef int xp_int32_t;
 	typedef unsigned int xp_uint32_t;
-#elif SIZEOF_LONG == 4
+#elif XP_SIZEOF_LONG == 4
 	typedef long xp_int32_t;
 	typedef unsigned long xp_uint32_t;
-#elif SIZEOF___INT32 == 4
+#elif XP_SIZEOF___INT32 == 4
 	typedef __int32 xp_int32_t;
 	typedef unsigned __int32 xp_uint32_t;
 #endif
 
-#if SIZEOF_INT == 8
+#if XP_SIZEOF_INT == 8
 	#define XP_HAVE_INT64_T
 	#define XP_HAVE_UINT64_T
 	typedef int xp_int64_t;
 	typedef unsigned int xp_uint64_t;
-#elif SIZEOF_LONG == 8
+#elif XP_SIZEOF_LONG == 8
 	#define XP_HAVE_INT64_T
 	#define XP_HAVE_UINT64_T
 	typedef long xp_int64_t;
 	typedef unsigned long xp_uint64_t;
-#elif SIZEOF_LONG_LONG == 8
+#elif XP_SIZEOF_LONG_LONG == 8
 	#define XP_HAVE_INT64_T
 	#define XP_HAVE_UINT64_T
 	typedef long long xp_int64_t;
 	typedef unsigned long long xp_uint64_t;
-#elif SIZEOF___INT64 == 8
+#elif XP_SIZEOF___INT64 == 8
 	#define XP_HAVE_INT64_T
 	#define XP_HAVE_UINT64_T
 	typedef __int64 xp_int64_t;
 	typedef unsigned __int64 xp_uint64_t;
 #endif
 
-#if SIZEOF_INT == 16
+#if XP_SIZEOF_INT == 16
 	#define XP_HAVE_INT128_T
 	#define XP_HAVE_UINT128_T
 	typedef int xp_int128_t;
 	typedef unsigned int xp_uint128_t;
-#elif SIZEOF_LONG == 16
+#elif XP_SIZEOF_LONG == 16
 	#define XP_HAVE_INT128_T
 	#define XP_HAVE_UINT128_T
 	typedef long xp_int128_t;
 	typedef unsigned long xp_uint128_t;
-#elif SIZEOF_LONG_LONG == 16
+#elif XP_SIZEOF_LONG_LONG == 16
 	#define XP_HAVE_INT128_T
 	#define XP_HAVE_UINT128_T
 	typedef long long xp_int128_t;
 	typedef unsigned long long xp_uint128_t;
-#elif SIZEOF___INT128 == 16
+#elif XP_SIZEOF___INT128 == 16
 	#define XP_HAVE_INT128_T
 	#define XP_HAVE_UINT128_T
 	typedef __int128 xp_int128_t;
@@ -196,7 +196,7 @@ typedef xp_int_t   xp_ssize_t;
 typedef xp_uint_t  xp_word_t;
 
 /* floating-point number */
-#if SIZEOF_LONG_DOUBLE > SIZEOF_DOUBLE
+#if XP_SIZEOF_LONG_DOUBLE > XP_SIZEOF_DOUBLE
 	typedef long double xp_real_t;
 #else
 	typedef double xp_real_t;
@@ -212,7 +212,7 @@ typedef int  xp_mcint_t;
 #elif defined(vms) || defined(__vms)
 	typedef long xp_wchar_t;
 	typedef long xp_wcint_t;
-#elif SIZEOF_LONG == 4
+#elif XP_SIZEOF_LONG == 4
 	typedef long xp_wchar_t;
 	typedef long xp_wcint_t;
 #else
