160edf1c46ef0210fef7c21bd416ac43a2a16fde
hyung-hwan <hyunghwan.chung@gmail.com>
2006-08-02 15:19:59 +0000

    *** empty log message ***

--- ase/awk/jni.c
+++ ase/awk/jni.c
@@ -1,17 +1,17 @@
 /*
- * $Id: jni.c,v 1.1 2006-08-02 15:03:48 bacon Exp $
+ * $Id: jni.c,v 1.2 2006-08-02 15:19:59 bacon Exp $
  */
 
 #include "jni.h"
 #include "awk.h"
 #include "sa.h"
 
-JNIEXPORT void JNICALL Java_AWK_open (JNIEnv* env, jobject obj)
+JNIEXPORT void JNICALL Java_xpkit_xpj_awk_AWK_open (JNIEnv* env, jobject obj)
 {
 	xp_printf (XP_T("Java_AWK_open\n"));
 }
 
-JNIEXPORT void JNICALL Java_AWK_close (JNIEnv* env, jobject obj)
+JNIEXPORT void JNICALL Java_xpkit_xpj_awk_AWK_close (JNIEnv* env, jobject obj)
 {
 	xp_printf (XP_T("Java_AWK_close\n"));
 }
