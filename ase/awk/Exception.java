ad54362925051ebd3349fbf0f0e8f9311bb49fe9
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2006-10-24 06:03:14 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@866 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ ase/awk/Exception.java
@@ -0,0 +1,18 @@
+/*
+ * $Id: Exception.java,v 1.1 2006-10-24 06:00:36 bacon Exp $
+ */
+
+package ase.awk;
+
+public class Exception extends java.lang.Exception
+{
+	Exception () 
+	{
+		super ();
+	}
+
+	Exception (String msg)
+	{
+		super (msg);
+	}
+}
