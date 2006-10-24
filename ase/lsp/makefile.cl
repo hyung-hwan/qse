50bb53ff527a51816d55f9507c214c327867696c
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2006-10-24 04:31:07 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@863 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/lsp/makefile.cl
+++ ase/lsp/makefile.cl
@@ -2,7 +2,7 @@ SRCS = lsp.c name.c token.c array.c mem.c env.c err.c \
 	read.c eval.c print.c \
 	prim.c prim_prog.c prim_let.c prim_compar.c prim_math.c
 OBJS = $(SRCS:.c=.obj)
-OUT = sselsp.lib
+OUT = aselsp.lib
 
 CC = cl
 LD = link
--- ase/types.h
+++ ase/types.h
@@ -1,268 +1,207 @@
 /*
- * $Id: types.h,v 1.57 2006-10-22 11:44:46 bacon Exp $
+ * $Id: types.h,v 1.58 2006-10-24 04:30:13 bacon Exp $
  */
 
-#ifndef _SSE_TYPES_H_
-#define _SSE_TYPES_H_
+#ifndef _ASE_TYPES_H_
+#define _ASE_TYPES_H_
 
 #if defined(_WIN32)
-	#include <sse/conf_msw.h>
+	#include <ase/conf_msw.h>
 #elif defined(vms) || defined(__vms)
-	#include <sse/conf_vms.h>
+	#include <ase/conf_vms.h>
 #elif defined(dos) || defined(__dos)
-	#include <sse/conf_dos.h>
+	#include <ase/conf_dos.h>
 #else
-	#include <sse/config.h>
+	#include <ase/config.h>
 #endif
 
-/*
- * NOTE: Data Types
- *   Various basic data types are predefined for convenience sake and used
- *   constantly thoughout the entire toolkit. The developers are strongly
- *   encouraged to use these data types rather than built-in data types
- *   supported by compilers. 
- *
- * NOTE: Availabilty 
- *   Some of the data types may or may not be available depending on
- *   the capability of the compiler.
- *
- * GROUP: State Types
- *
- * TYPE: sse_bool_t
- *   Boolean type
- *
- * TYPE: sse_tri_t
- *   Tri-State type
- *
- * GROUP: Integral Types
- *
- * TYPE: sse_byte_t
- * 
- * TYPE: sse_word_t
- *
- * TYPE: sse_int_t
- *   Signed integer whose size equals the size of a pointer
- *
- * TYPE: sse_uint_t
- *   Unsigned integer whose size equals the size of a pointer
- * 
- * TYPE: sse_long_t
- *   The largest signed integer available
- *
- * TYPE: sse_ulong_t
- *   The largest unsigned integer available
- * 
- * TYPE: sse_size_t
- *   Unsigned integer which can represent the size of the largest 
- *   in-memory data
- *
- * TYPE: sse_ssize_t
- *   Signed version of sse_size_t
- *
- * TYPE: sse_int8_t
- *
- * TYPE: sse_uint8_t
- *
- * TYPE: sse_int16_t
- *
- * TYPE: sse_uint16_t
- *
- * TYPE: sse_int32_t
- * 
- * TYPE: sse_uint32_t
- *
- * TYPE: sse_int64_t
- *
- * TYPE: sse_uint64_t
- */
-
 /* boolean type */
-typedef int sse_bool_t;
-#define sse_true  (0 == 0)
-#define sse_false (0 != 0)
+typedef int ase_bool_t;
+#define ase_true  (0 == 0)
+#define ase_false (0 != 0)
 
 /* tri-state type */
-typedef int sse_tri_t;
-#define sse_alive   1
-#define sse_zombie  0
-#define sse_dead   -1
+typedef int ase_tri_t;
+#define ase_alive   1
+#define ase_zombie  0
+#define ase_dead   -1
 
 /* integer that can hold a pointer */
-#if SSE_SIZEOF_VOID_P == SSE_SIZEOF_INT
-	typedef int sse_int_t;
-	typedef unsigned int sse_uint_t;
-#elif SSE_SIZEOF_VOID_P == SSE_SIZEOF_LONG
-	typedef long sse_int_t;
-	typedef unsigned long sse_uint_t;
-#elif SSE_SIZEOF_VOID_P == SSE_SIZEOF_LONG_LONG
-	typedef long long sse_int_t;
-	typedef unsigned long long sse_uint_t;
+#if ASE_SIZEOF_VOID_P == ASE_SIZEOF_INT
+	typedef int ase_int_t;
+	typedef unsigned int ase_uint_t;
+#elif ASE_SIZEOF_VOID_P == ASE_SIZEOF_LONG
+	typedef long ase_int_t;
+	typedef unsigned long ase_uint_t;
+#elif ASE_SIZEOF_VOID_P == ASE_SIZEOF_LONG_LONG
+	typedef long long ase_int_t;
+	typedef unsigned long long ase_uint_t;
 #else
 	#error Unsupported pointer size
 #endif
 
 
 /* the largest integer supported by the system */
-#if SSE_SIZEOF_LONG_LONG != 0
-	typedef long long sse_long_t;
-	typedef unsigned long long sse_ulong_t;
-#elif SSE_SIZEOF___INT64 != 0
-	typedef __int64 sse_long_t;
-	typedef unsigned __int64 sse_ulong_t;
+#if ASE_SIZEOF_LONG_LONG != 0
+	typedef long long ase_long_t;
+	typedef unsigned long long ase_ulong_t;
+#elif ASE_SIZEOF___INT64 != 0
+	typedef __int64 ase_long_t;
+	typedef unsigned __int64 ase_ulong_t;
 #else
-	typedef long sse_long_t;
-	typedef unsigned long sse_ulong_t;
+	typedef long ase_long_t;
+	typedef unsigned long ase_ulong_t;
 #endif
 
 /* integers of specific size */
-#if SSE_SIZEOF_CHAR == 1
-	typedef char sse_int8_t;
-	typedef unsigned char sse_uint8_t;
-#elif SSE_SIZEOF___INT8 == 1
-	typedef __int8 sse_int8_t;
-	typedef unsigned __int8 sse_uint8_t;
+#if ASE_SIZEOF_CHAR == 1
+	typedef char ase_int8_t;
+	typedef unsigned char ase_uint8_t;
+#elif ASE_SIZEOF___INT8 == 1
+	typedef __int8 ase_int8_t;
+	typedef unsigned __int8 ase_uint8_t;
 #endif
 
-#if SSE_SIZEOF_SHORT == 2
-	typedef short sse_int16_t;
-	typedef unsigned short sse_uint16_t;
-#elif SSE_SIZEOF___INT16 == 2
-	typedef __int16 sse_int16_t;
-	typedef unsigned __int16 sse_uint16_t;
+#if ASE_SIZEOF_SHORT == 2
+	typedef short ase_int16_t;
+	typedef unsigned short ase_uint16_t;
+#elif ASE_SIZEOF___INT16 == 2
+	typedef __int16 ase_int16_t;
+	typedef unsigned __int16 ase_uint16_t;
 #endif
 
-#if SSE_SIZEOF_INT == 4
-	typedef int sse_int32_t;
-	typedef unsigned int sse_uint32_t;
-#elif SSE_SIZEOF_LONG == 4
-	typedef long sse_int32_t;
-	typedef unsigned long sse_uint32_t;
-#elif SSE_SIZEOF___INT32 == 4
-	typedef __int32 sse_int32_t;
-	typedef unsigned __int32 sse_uint32_t;
+#if ASE_SIZEOF_INT == 4
+	typedef int ase_int32_t;
+	typedef unsigned int ase_uint32_t;
+#elif ASE_SIZEOF_LONG == 4
+	typedef long ase_int32_t;
+	typedef unsigned long ase_uint32_t;
+#elif ASE_SIZEOF___INT32 == 4
+	typedef __int32 ase_int32_t;
+	typedef unsigned __int32 ase_uint32_t;
 #endif
 
-#if SSE_SIZEOF_INT == 8
-	#define SSE_HAVE_INT64_T
-	#define SSE_HAVE_UINT64_T
-	typedef int sse_int64_t;
-	typedef unsigned int sse_uint64_t;
-#elif SSE_SIZEOF_LONG == 8
-	#define SSE_HAVE_INT64_T
-	#define SSE_HAVE_UINT64_T
-	typedef long sse_int64_t;
-	typedef unsigned long sse_uint64_t;
-#elif SSE_SIZEOF_LONG_LONG == 8
-	#define SSE_HAVE_INT64_T
-	#define SSE_HAVE_UINT64_T
-	typedef long long sse_int64_t;
-	typedef unsigned long long sse_uint64_t;
-#elif SSE_SIZEOF___INT64 == 8
-	#define SSE_HAVE_INT64_T
-	#define SSE_HAVE_UINT64_T
-	typedef __int64 sse_int64_t;
-	typedef unsigned __int64 sse_uint64_t;
+#if ASE_SIZEOF_INT == 8
+	#define ASE_HAVE_INT64_T
+	#define ASE_HAVE_UINT64_T
+	typedef int ase_int64_t;
+	typedef unsigned int ase_uint64_t;
+#elif ASE_SIZEOF_LONG == 8
+	#define ASE_HAVE_INT64_T
+	#define ASE_HAVE_UINT64_T
+	typedef long ase_int64_t;
+	typedef unsigned long ase_uint64_t;
+#elif ASE_SIZEOF_LONG_LONG == 8
+	#define ASE_HAVE_INT64_T
+	#define ASE_HAVE_UINT64_T
+	typedef long long ase_int64_t;
+	typedef unsigned long long ase_uint64_t;
+#elif ASE_SIZEOF___INT64 == 8
+	#define ASE_HAVE_INT64_T
+	#define ASE_HAVE_UINT64_T
+	typedef __int64 ase_int64_t;
+	typedef unsigned __int64 ase_uint64_t;
 #endif
 
-#if SSE_SIZEOF_INT == 16
-	#define SSE_HAVE_INT128_T
-	#define SSE_HAVE_UINT128_T
-	typedef int sse_int128_t;
-	typedef unsigned int sse_uint128_t;
-#elif SSE_SIZEOF_LONG == 16
-	#define SSE_HAVE_INT128_T
-	#define SSE_HAVE_UINT128_T
-	typedef long sse_int128_t;
-	typedef unsigned long sse_uint128_t;
-#elif SSE_SIZEOF_LONG_LONG == 16
-	#define SSE_HAVE_INT128_T
-	#define SSE_HAVE_UINT128_T
-	typedef long long sse_int128_t;
-	typedef unsigned long long sse_uint128_t;
-#elif SSE_SIZEOF___INT128 == 16
-	#define SSE_HAVE_INT128_T
-	#define SSE_HAVE_UINT128_T
-	typedef __int128 sse_int128_t;
-	typedef unsigned __int128 sse_uint128_t;
+#if ASE_SIZEOF_INT == 16
+	#define ASE_HAVE_INT128_T
+	#define ASE_HAVE_UINT128_T
+	typedef int ase_int128_t;
+	typedef unsigned int ase_uint128_t;
+#elif ASE_SIZEOF_LONG == 16
+	#define ASE_HAVE_INT128_T
+	#define ASE_HAVE_UINT128_T
+	typedef long ase_int128_t;
+	typedef unsigned long ase_uint128_t;
+#elif ASE_SIZEOF_LONG_LONG == 16
+	#define ASE_HAVE_INT128_T
+	#define ASE_HAVE_UINT128_T
+	typedef long long ase_int128_t;
+	typedef unsigned long long ase_uint128_t;
+#elif ASE_SIZEOF___INT128 == 16
+	#define ASE_HAVE_INT128_T
+	#define ASE_HAVE_UINT128_T
+	typedef __int128 ase_int128_t;
+	typedef unsigned __int128 ase_uint128_t;
 #endif
 
 /* miscellaneous integral types */
-typedef sse_uint8_t sse_byte_t;
-typedef sse_uint_t  sse_size_t;
-typedef sse_int_t   sse_ssize_t;
-typedef sse_uint_t  sse_word_t;
+typedef ase_uint8_t ase_byte_t;
+typedef ase_uint_t  ase_size_t;
+typedef ase_int_t   ase_ssize_t;
+typedef ase_uint_t  ase_word_t;
 
 /* floating-point number */
 #if defined(__FreeBSD__)
 	/* TODO: check if the support for long double is complete.
-	 *       if so, use long double for sse_real_t */
-	#define SSE_SIZEOF_REAL SSE_SIZEOF_DOUBLE
-	typedef double sse_real_t;
-#elif SSE_SIZEOF_LONG_DOUBLE > SSE_SIZEOF_DOUBLE
-	#define SSE_SIZEOF_REAL SSE_SIZEOF_LONG_DOUBLE
-	typedef long double sse_real_t;
+	 *       if so, use long double for ase_real_t */
+	#define ASE_SIZEOF_REAL ASE_SIZEOF_DOUBLE
+	typedef double ase_real_t;
+#elif ASE_SIZEOF_LONG_DOUBLE > ASE_SIZEOF_DOUBLE
+	#define ASE_SIZEOF_REAL ASE_SIZEOF_LONG_DOUBLE
+	typedef long double ase_real_t;
 #else
-	#define SSE_SIZEOF_REAL SSE_SIZEOF_DOUBLE
-	typedef double sse_real_t;
+	#define ASE_SIZEOF_REAL ASE_SIZEOF_DOUBLE
+	typedef double ase_real_t;
 #endif
 
 /* character types */
-typedef char sse_mchar_t;
-typedef int  sse_mcint_t;
+typedef char ase_mchar_t;
+typedef int  ase_mcint_t;
 
 #if defined(__cplusplus)
 	/* C++ */
-	typedef wchar_t sse_wchar_t;
-	typedef wchar_t sse_wcint_t;
+	typedef wchar_t ase_wchar_t;
+	typedef wchar_t ase_wcint_t;
 
 	/* all the way down from here for C */
-#elif (SSE_SIZEOF_WCHAR_T == 2) || (SSE_SIZEOF_WCHAR_T == 0)
-	typedef unsigned short sse_wchar_t;
-	typedef unsigned short sse_wcint_t;
-#elif (SSE_SIZEOF_WCHAR_T == 4)
+#elif (ASE_SIZEOF_WCHAR_T == 2) || (ASE_SIZEOF_WCHAR_T == 0)
+	typedef unsigned short ase_wchar_t;
+	typedef unsigned short ase_wcint_t;
+#elif (ASE_SIZEOF_WCHAR_T == 4)
 	#if defined(vms) || defined(__vms)
-		typedef unsigned int sse_wchar_t;
-		typedef int sse_wcint_t;
+		typedef unsigned int ase_wchar_t;
+		typedef int ase_wcint_t;
 	#elif defined(__FreeBSD__)
-		typedef int sse_wchar_t;
-		typedef int sse_wcint_t;
+		typedef int ase_wchar_t;
+		typedef int ase_wcint_t;
 	#elif (defined(sun) || defined(__sun)) && defined(_LP64)
-		typedef int sse_wchar_t;
-		typedef int sse_wcint_t;
-	#elif SSE_SIZEOF_LONG == 4
-		typedef long sse_wchar_t;
-		typedef long sse_wcint_t;
+		typedef int ase_wchar_t;
+		typedef int ase_wcint_t;
+	#elif ASE_SIZEOF_LONG == 4
+		typedef long ase_wchar_t;
+		typedef long ase_wcint_t;
 	#else
-		typedef int sse_wchar_t;
-		typedef int sse_wcint_t;
+		typedef int ase_wchar_t;
+		typedef int ase_wcint_t;
 	#endif
 #else
 	#error unsupported size of wchar_t
 #endif
 
 #if defined(_WIN32) && (defined(UNICODE)||defined(_UNICODE))
-	#define SSE_CHAR_IS_WCHAR
-	typedef sse_wchar_t sse_char_t;
-	typedef sse_wcint_t sse_cint_t;
+	#define ASE_CHAR_IS_WCHAR
+	typedef ase_wchar_t ase_char_t;
+	typedef ase_wcint_t ase_cint_t;
 #else
-	#if defined(SSE_CHAR_IS_MCHAR)
-		typedef sse_mchar_t sse_char_t;
-		typedef sse_mcint_t sse_cint_t;
-	#elif defined(SSE_CHAR_IS_WCHAR)
-		typedef sse_wchar_t sse_char_t;
-		typedef sse_wcint_t sse_cint_t;
+	#if defined(ASE_CHAR_IS_MCHAR)
+		typedef ase_mchar_t ase_char_t;
+		typedef ase_mcint_t ase_cint_t;
+	#elif defined(ASE_CHAR_IS_WCHAR)
+		typedef ase_wchar_t ase_char_t;
+		typedef ase_wcint_t ase_cint_t;
 	#elif defined(_MBCS)
-		#define SSE_CHAR_IS_MCHAR
-		typedef sse_mchar_t sse_char_t;
-		typedef sse_mcint_t sse_cint_t;
+		#define ASE_CHAR_IS_MCHAR
+		typedef ase_mchar_t ase_char_t;
+		typedef ase_mcint_t ase_cint_t;
 	#else
-		#define SSE_CHAR_IS_WCHAR
-		typedef sse_wchar_t sse_char_t;
-		typedef sse_wcint_t sse_cint_t;
+		#define ASE_CHAR_IS_WCHAR
+		typedef ase_wchar_t ase_char_t;
+		typedef ase_wcint_t ase_cint_t;
 	#endif
 #endif
 
-#if defined(SSE_CHAR_IS_WCHAR) && defined(_WIN32) 
+#if defined(ASE_CHAR_IS_WCHAR) && defined(_WIN32) 
 	#ifndef UNICODE
 		#define UNICODE
 	#endif
