a8f2f8b5fbe3fc90ae27184612731a3afc15095c
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2006-11-19 07:45:46 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@941 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/test/awk/regress.sh
+++ ase/test/awk/regress.sh
@@ -1,21 +1,57 @@
 #!/bin/sh
 
-pid=$$
+init()
+{
+	for script in emp-???.awk
+	do
+		output=`echo $script | sed 's/\.awk$/.out/g'`
+		./awk $script emp-en.data > "$output"
+	done
+}
 
-for script in emp???.awk
-do
-	output=`echo $script | sed 's/\.awk$/.out/g'`
-	./awk $script emp-en.data > "$output.$pid"
+test()
+{
+	pid=$$
 
-	diff $output "$output.$pid" 
-	if [ $? -ne 0 ]
-	then
-		echo "###################################"
-		echo "PROBLEM(S) DETECTED IN $script.".
-		echo "###################################"
+	for script in emp-???.awk
+	do
+		output=`echo $script | sed 's/\.awk$/.out/g'`
+		./awk $script emp-en.data > "$output.$pid"
+
+		diff $output "$output.$pid" 
+		if [ $? -ne 0 ]
+		then
+			echo "###################################"
+			echo "PROBLEM(S) DETECTED IN $script.".
+			echo "###################################"
+			rm -f "$output.$pid"
+			break
+		fi
+
 		rm -f "$output.$pid"
-		break
-	fi
+	done
+}
 
-	rm -f "$output.$pid"
-done
+#--------#
+#  main  #
+#--------#
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 init"
+	echo "       $0 test"
+	exit 1
+fi
+
+if [ "$1" = "init" ]
+then
+	init	
+elif [ "$1" = "test" ]
+then
+	test
+else
+	echo "Usage: $0 init"
+	echo "       $0 test"
+	exit 1
+fi
+
