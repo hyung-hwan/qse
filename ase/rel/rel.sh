8b7bf52cec7bee8a53e3c8acb2c0137fe1f235bd
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-02-04 13:30:35 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1147 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/rel/rel.sh
+++ ase/rel/rel.sh
@@ -16,6 +16,7 @@ finalize ()
 		if [ "$i" = "*" ]; then continue; fi
 		if [ "$i" = "CVS" ]; then continue; fi
 		if [ "$i" = "stx" ]; then continue; fi
+		if [ "$i" = "web.out" ]; then continue; fi
 
 		if [ "$cur" = "" ] 
 		then
@@ -39,22 +40,23 @@ finalize ()
 			cur="$1"; dir="$2";
 		elif [ -f "$full" ]
 		then
-			root="$BASE/ase-$VER"
-			target="$root/ase"
+			target="$SOURCE_ROOT/ase"
 			mkdir -p "$target/$cur"
 
-			case "$full" in
+			case "$i" in
 			*.h|*.c|*.cc|*.cpp|*.java|*.awk|*.in)
 				"$AWK" -f "$BASE/rel/lic.awk" -a "$target/$file" "$full"
 				;;
 			*.man)
-				tmp=`echo $i | sed 's/.man$/.html/'`
-				"$AWK" -f "$BASE/rel/doc.awk" "$full" > "$root/$tmp"
+				html=`echo $i | sed 's/.man$/.html/'`
+				"$AWK" -f "$BASE/rel/doc.awk" "$full" > "$SOURCE_ROOT/html/$html"
+				"$AWK" -f "$BASE/rel/doc.awk" "$full" > "$DEPLOY_ROOT/$html"
 				cp -f "$full" "$target/$file"
 				;;
 			*.css)
 				cp -f "$full" "$target/$file"
-				cp -f "$full" "$root/$file"
+				cp -f "$full" "$SOURCE_ROOT/html/$i"
+				cp -f "$full" "$DEPLOY_ROOT/$i"
 				;;
 			*)
 				cp -f "$full" "$target/$file"
@@ -67,18 +69,29 @@ finalize ()
 if [ ! -f ../CVS/Tag ]
 then
 	echo "Error: ../CVS/Tag not found"
-	#exit 1;
+	exit 1;
 fi
 
 AWK="$HOME/awk"
 
-VER=`cat ../CVS/Tag | cut -c6- | tr '[A-Z]' '[a-z]' | sed 's/_/./g`
-VER="0.1.0"
+VERSION=`cat ../CVS/Tag | cut -c6- | tr '[A-Z]' '[a-z]' | sed 's/_/./g`
 
 cwd=`pwd`
 cd ".."
 BASE=`pwd`
+DEPLOY_ROOT="$BASE/web.out"
+SOURCE_ROOT="$DEPLOY_ROOT/ase-$VERSION"
+
+rm -rf "$DEPLOY_ROOT"
+mkdir -p "$DEPLOY_ROOT"
+mkdir -p "$SOURCE_ROOT"
+mkdir -p "$SOURCE_ROOT/html"
 
 finalize "" ""
+
+cd "$DEPLOY_ROOT"
+tar -cvf "ase-$VERSION.tar" "ase-$VERSION"
+gzip "ase-$VERSION.tar"
+rm -rf "ase-$VERSION"
 
 exit 0
