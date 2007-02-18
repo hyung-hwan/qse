e084050680f75d781fdaa91e84e06636f58ddde7
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-02-18 15:51:07 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1196 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/awk/rex.c
+++ ase/awk/rex.c
@@ -1,5 +1,5 @@
 /*
- * $Id: rex.c,v 1.62 2007-02-18 13:07:00 bacon Exp $
+ * $Id: rex.c,v 1.63 2007-02-18 15:51:07 bacon Exp $
  *
  * {License}
  */
@@ -53,17 +53,15 @@ typedef struct __builder_t __builder_t;
 typedef struct __matcher_t __matcher_t;
 typedef struct __match_t __match_t;
 
-struct __code_t
-{
+ASE_BEGIN_PACKED_STRUCT(__code_t)
 	/*ase_byte_t cmd;*/
 	short cmd;
 	short negate; /* only for CMD_CHARSET */
 	ase_size_t lbound;
 	ase_size_t ubound;
-} __attribute__((packed));
+ASE_END_PACKED_STRUCT()
 
-struct __builder_t
-{
+ASE_BEGIN_PACKED_STRUCT(__builder_t)
 	ase_awk_t* awk;
 
 	struct
@@ -92,10 +90,9 @@ struct __builder_t
 	} depth;
 
 	int errnum;
-} __attribute__((packed));
+ASE_END_PACKED_STRUCT()
 
-struct __matcher_t
-{
+ASE_BEGIN_PACKED_STRUCT(__matcher_t)
 	ase_awk_t* awk;
 
 	struct
@@ -115,10 +112,9 @@ struct __matcher_t
 
 	int ignorecase;
 	int errnum;
-} __attribute__((packed));
+ASE_END_PACKED_STRUCT()
 
-struct __match_t
-{
+ASE_BEGIN_PACKED_STRUCT(__match_t)
 	const ase_char_t* match_ptr;
 
 	ase_bool_t matched;
@@ -126,7 +122,7 @@ struct __match_t
 
 	const ase_byte_t* branch;
 	const ase_byte_t* branch_end;
-} __attribute__((packed));
+ASE_END_PACKED_STRUCT()
 
 typedef const ase_byte_t* (*atom_matcher_t) (
 	__matcher_t* matcher, const ase_byte_t* base, __match_t* mat);
---
+++ ase/unpack.h
@@ -0,0 +1,11 @@
+/*
+ * $Id: unpack.h,v 1.1 2007-02-18 15:51:07 bacon Exp $
+ *
+ * {License}
+ */
+
+#if defined(__GNUC__)
+#pragma pack(pop)
+#elif defined(__HP_aCC) || defined(__HP_cc)
+#pragma pack
+#endif
