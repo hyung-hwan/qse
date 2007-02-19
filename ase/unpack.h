f6214b7fcd1f76646ba73a8d99bcd1941bdf2a87
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-02-19 06:13:03 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1200 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/unpack.h
+++ ase/unpack.h
@@ -1,13 +1,15 @@
 /*
- * $Id: unpack.h,v 1.3 2007-02-18 16:49:03 bacon Exp $
+ * $Id: unpack.h,v 1.4 2007-02-19 06:13:03 bacon Exp $
  *
  * {License}
  */
 
 #if defined(__GNUC__)
-#pragma pack(pop)
+	#pragma pack(pop)
 #elif defined(__HP_aCC) || defined(__HP_cc)
-#pragma PACK
+	#pragma PACK
 #elif defined(__MSC_VER) || defined(__BORLANDC__)
-#pragma pack(pop)
+	#pragma pack(pop)
+#elif defined(__DECC)
+	#pragma pack(pop)
 #endif
