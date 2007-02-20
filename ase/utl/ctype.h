db243a70d5dd5edd3d2e54c3a5eaf1436ffb8152
hyung-hwan <hyunghwan.chung@gmail.com>
2007-02-20 14:50:35 +0000

    *** empty log message ***

--- ase/utl/ctype.h
+++ ase/utl/ctype.h
@@ -10,21 +10,26 @@
 
 #if defined(ASE_CHAR_IS_MCHAR) 
 	#include <ctype.h>
+	
+	#if !defined(isupper) 
+		#define ase_isupper  isupper
+		#define ase_islower  islower
+		#define ase_isalpha  isalpha
+		#define ase_isdigit  isdigit
+		#define ase_isxdigit isxdigit
+		#define ase_isalnum  isalnum
+		#define ase_isspace  isspace
+		#define ase_isprint  isprint
+		#define ase_isgraph  isgraph
+		#define ase_iscntrl  iscntrl
+		#define ase_ispunct  ispunct
+	#endif
 
-	#define ase_isupper  isupper
-	#define ase_islower  islower
-	#define ase_isalpha  isalpha
-	#define ase_isdigit  isdigit
-	#define ase_isxdigit isxdigit
-	#define ase_isalnum  isalnum
-	#define ase_isspace  isspace
-	#define ase_isprint  isprint
-	#define ase_isgraph  isgraph
-	#define ase_iscntrl  iscntrl
-	#define ase_ispunct  ispunct
+	#if !defined(toupper)
+		#define ase_toupper  toupper
+		#define ase_tolower  tolower
+	#endif
 
-	#define ase_toupper  tolower
-	#define ase_tolower  tolower
 #else
 	#include <wctype.h>
 
@@ -40,8 +45,35 @@
 	#define ase_iscntrl  iswcntrl
 	#define ase_ispunct  iswpunct
 
-	#define ase_toupper  towlower
+	#define ase_toupper  towupper
 	#define ase_tolower  towlower
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(ASE_CHAR_IS_MCHAR) && defined(isupper)
+	int ase_isupper (int c);
+	int ase_islower (int c);
+	int ase_isalpha (int c);
+	int ase_isdigit (int c);
+	int ase_isxdigit (int c);
+	int ase_isalnum (int c);
+	int ase_isspace (int c);
+	int ase_isprint (int c);
+	int ase_isgraph (int c);
+	int ase_iscntrl (int c);
+	int ase_ispunct (int c);
+#endif
+
+#if defined(ASE_CHAR_IS_MCHAR) && defined(toupper)
+	int ase_toupper (int c);
+	int ase_tolower (int c);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
