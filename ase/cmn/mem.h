d768e7c29864374d81cb9e9fc1f142b2a4fb12bd
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-02-23 07:13:58 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1219 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ ase/cmn/mem.h
@@ -0,0 +1,25 @@
+/*
+ * $Id: mem.h,v 1.1 2007-02-23 07:13:58 bacon Exp $
+ *
+ * {License}
+ */
+
+#ifndef _ASE_CMN_MEM_H_
+#define _ASE_CMN_MEM_H_
+
+#include <ase/types.h>
+#include <ase/macros.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void* ase_memcpy (void* dst, const void* src, ase_size_t n);
+void* ase_memset (void* dst, int val, ase_size_t n);
+int ase_memcmp (const void* s1, const void* s2, ase_size_t n);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
