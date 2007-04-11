c097d150005c1fb3a832a76889fdd9373124c1a0
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-04-11 09:39:46 +0000

    *** empty log message ***
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1358 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/test/awk/AseAwkApplet.java
+++ ase/test/awk/AseAwkApplet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: AseAwkApplet.java,v 1.1 2007-04-10 08:01:34 bacon Exp $
+ * $Id: AseAwkApplet.java,v 1.2 2007-04-11 09:39:46 bacon Exp $
  */
 
 import java.applet.*;
@@ -9,19 +9,62 @@ import java.net.URL;
 
 public class AseAwkApplet extends Applet
 {
+	private TextArea srcIn;
+	private TextArea srcOut;
+	private TextArea conIn;
+	private TextArea conOut;
+	private TextField jniLib;
+
 	public void init () 
 	{
-		Button btn_run;
-		btn_run = new Button ("Run Awk");
+		jniLib = new TextField ();
 
-		btn_run.addActionListener (new ActionListener ()
+		srcIn = new TextArea ();
+		srcOut = new TextArea ();
+		conIn = new TextArea ();
+		conOut = new TextArea ();
+
+		Button runBtn = new Button ("Run Awk");
+
+		runBtn.addActionListener (new ActionListener ()
 		{
 			public void actionPerformed (ActionEvent e)
 			{
 				run_awk ();
 			}
 		});
-		add (btn_run);
+
+		Panel topPanel = new Panel ();
+		BorderLayout topPanelLayout = new BorderLayout ();
+		topPanel.setLayout (topPanelLayout);
+
+		topPanelLayout.setHgap (2);
+		topPanelLayout.setVgap (2);
+		topPanel.add (new Label ("JNI Library: "), BorderLayout.WEST);
+		topPanel.add (jniLib, BorderLayout.CENTER);
+
+		Panel centerPanel = new Panel ();
+		GridLayout centerPanelLayout = new GridLayout (2, 2);
+
+		centerPanel.setLayout (centerPanelLayout);
+
+		centerPanelLayout.setHgap (2);
+		centerPanelLayout.setVgap (2);
+
+		centerPanel.add (srcIn);
+		centerPanel.add (srcOut);
+		centerPanel.add (conIn);
+		centerPanel.add (conOut);
+
+		BorderLayout mainLayout = new BorderLayout ();
+		mainLayout.setHgap (2);
+		mainLayout.setVgap (2);
+
+		setLayout (mainLayout);
+		
+		add (topPanel, BorderLayout.NORTH);
+		add (centerPanel, BorderLayout.CENTER);
+		add (runBtn, BorderLayout.SOUTH);
 	}
 
 	public void stop () {}
