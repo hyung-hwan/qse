515ef36507dd43d2d5c768f9a927d7cce8af64d0
hyung-hwan <hyunghwan.chung@gmail.com>
2007-04-26 15:43:06 +0000

    *** empty log message ***

--- ase/test/cnt/Properties/Settings.Designer.cs
+++ ase/test/cnt/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ASETestCom.Properties {
+namespace ASETestCNT.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
