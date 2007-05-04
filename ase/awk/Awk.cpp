6f28e48ff984ad8d8a22bdae78bbe901a95f0e4b
hyung-hwan <hyunghwan.chung@gmail.com>
2007-05-04 00:14:00 +0000

    Recovered from cvs revision 2007-05-03 15:14:00

--- ase/awk/Awk.cpp
+++ ase/awk/Awk.cpp
@@ -1,23 +1,18 @@
 /*
- * $Id: Awk.cpp,v 1.2 2007/05/01 12:39:22 bacon Exp $
+ * $Id: Awk.cpp,v 1.4 2007/05/02 15:07:33 bacon Exp $
  */
 
 #include <ase/awk/Awk.hpp>
 
 namespace ASE
 {
-
 	Awk::Awk (): awk (ASE_NULL)
 	{
 	}
 
 	Awk::~Awk ()
 	{
-		if (awk != ASE_NULL) 
-		{
-			ase_awk_close (awk);
-			awk = ASE_NULL;
-		}
+		close ();
 	}
 
 	int Awk::parse ()
@@ -27,18 +22,76 @@ namespace ASE
 			/*awk = ase_awk_open (*/
 		}
 
+		ase_awk_srcios_t srcios;
+
+		srcios.in = sourceReader;
+		srcios.out = sourceWriter;
+		srcios.custom_data = this;
+
 		return ase_awk_parse (awk, ASE_NULL);
 	}
 
-	int Awk::run (/*const ase_char_t* main*/)
+	int Awk::run (const ase_char_t* main, const ase_char_t** args)
 	{
-		if (awk == ASE_NULL)
+		if (awk == ASE_NULL) 
 		{
+			// TODO: SET ERROR INFO
+			return -1;
 		}
 
 		//return ase_awk_run (awk, main);
 		return 0;
 	}
 
-}
+	void Awk::close ()
+	{
+		if (awk != ASE_NULL) 
+		{
+			ase_awk_close (awk);
+			awk = ASE_NULL;
+		}
+	}
 
+	ase_ssize_t Awk::sourceReader (
+		int cmd, void* arg, ase_char_t* data, ase_size_t count)
+	{
+		ASE::Awk* awk = (ASE::Awk*)arg;
+	
+		if (cmd == ASE_AWK_IO_OPEN) 
+		{
+			return awk->openSource (ASE::Awk::SOURCE_READ);
+		}
+		else if (cmd == ASE_AWK_IO_CLOSE)
+		{
+			return awk->closeSource (ASE::Awk::SOURCE_READ);
+		}
+		else if (cmd == ASE_AWK_IO_READ)
+		{
+			return awk->readSource (data, count);
+		}
+	
+		return -1;
+	}
+	
+	ase_ssize_t Awk::sourceWriter (
+		int cmd, void* arg, ase_char_t* data, ase_size_t count)
+	{
+		ASE::Awk* awk = (ASE::Awk*)arg;
+	
+		if (cmd == ASE_AWK_IO_OPEN) 
+		{
+			return awk->openSource (ASE::Awk::SOURCE_WRITE);
+		}
+		else if (cmd == ASE_AWK_IO_CLOSE)
+		{
+			return awk->closeSource (ASE::Awk::SOURCE_WRITE);
+		}
+		else if (cmd == ASE_AWK_IO_WRITE)
+		{
+			return awk->writeSource (data, count);
+		}
+	
+		return -1;
+	}
+	
+}
--- ase/awk/Awk.hpp
+++ ase/awk/Awk.hpp
@@ -1,5 +1,5 @@
 /*
- * $Id: Awk.hpp,v 1.1 2007/04/30 05:47:33 bacon Exp $
+ * $Id: Awk.hpp,v 1.3 2007/05/02 15:07:33 bacon Exp $
  */
 
 #ifndef _ASE_AWK_AWK_HPP_
@@ -14,10 +14,31 @@ namespace ASE
 	{
 	public:
 		Awk ();
-		~Awk ();
+		virtual ~Awk ();
 		
 		int parse ();
-		int run ();
+		int run (const ase_char_t* main, const ase_char_t** args);
+		void close ();
+
+		enum SourceMode
+		{
+			SOURCE_READ,
+			SOURCE_WRITE
+		};
+
+	protected:
+		virtual int openSource (SourceMode mode) = 0;
+		virtual int closeSource (SourceMode mode) = 0;
+
+		virtual ase_ssize_t readSource (
+			ase_char_t* buf, ase_size_t len) = 0;
+		virtual ase_ssize_t writeSource (
+			ase_char_t* buf, ase_size_t len) = 0;
+
+		static ase_ssize_t sourceReader (
+        		int cmd, void* arg, ase_char_t* data, ase_size_t count);
+		static ase_ssize_t sourceWriter (
+        		int cmd, void* arg, ase_char_t* data, ase_size_t count);
 
 	private:
 		ase_awk_t* awk;
