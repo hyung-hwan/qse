2713268391faf677806da5aeba9d8086d1e84e7b
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2007-07-10 01:07:00 +0000

    Recovered from cvs revision 2007-07-09 16:02:00
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@1455 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- ase/com/Awk.h
+++ ase/com/Awk.h
@@ -1,5 +1,5 @@
 /*
- * $Id: Awk.h,v 1.5 2007/06/29 11:24:27 bacon Exp $
+ * $Id: Awk.h,v 1.6 2007/07/02 14:04:20 bacon Exp $
  *
  * {License}
  */
@@ -173,6 +173,8 @@ public:
 	STDMETHOD(get_ErrorLine)(/*[out, retval]*/ int *pVal);
 	STDMETHOD(get_ErrorCode)(/*[out, retval]*/ int *pVal);
 
+	HRESULT __stdcall UnsetAllWords (
+		/*[out, retval]*/ VARIANT_BOOL* ret);
 	HRESULT __stdcall UnsetWord (
 		/*[in]*/ BSTR ow, /*[out, retval]*/ VARIANT_BOOL* ret);
 	HRESULT __stdcall SetWord (
