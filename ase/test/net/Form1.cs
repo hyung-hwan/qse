af7146069a694aa758ae033ceea9ab4aa7cb7617
hyung-hwan <hyunghwan.chung@gmail.com>
2007-07-17 20:20:00 +0000

    Recovered from cvs revision 2007-07-17 09:46:00

--- ase/test/net/Form1.cs
+++ ase/test/net/Form1.cs
@@ -17,7 +17,13 @@ namespace asetestnet
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			ASE.Net.Awk awk = new ASE.Net.Awk();
+			ASE.Net.Awk awk = new ASE.Net.StdAwk();
+
+			awk.Open();
+			awk.SourceInputStream = new System.IO.FileStream("t.awk", System.IO.FileMode.Open, System.IO.FileAccess.Read);
+			awk.SourceOutputStream = new System.IO.FileStream("t.out", System.IO.FileMode.Create, System.IO.FileAccess.Write);
+
+			awk.Parse();
 		}
 	}
 }
