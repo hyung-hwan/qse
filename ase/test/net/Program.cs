b9a4af9a4e6fe915df983ab2d4dc7e9421a78b40
hyung-hwan <hyunghwan.chung@gmail.com>
2007-08-05 23:52:00 +0000

    Recovered from cvs revision 2007-08-04 11:42:00

--- ase/test/net/Program.cs
+++ ase/test/net/Program.cs
@@ -14,7 +14,7 @@ namespace asetestnet
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new AwkForm());
 		}
 	}
 }
