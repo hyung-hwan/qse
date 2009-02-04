27c13aabf7bb40a905f7b2b2c5922786d0109b5f
hyung-hwan <hyunghwan.chung@gmail.com>
2009-02-04 08:29:11 +0000

    started working on a stream editor (qse_sed_t)

---
+++ qse/lib/utl/sed.c
@@ -0,0 +1,36 @@
+/*
+ * $Id$
+ *
+  ------------------------------------------------------------------------------
+   This file is based on Plan 9 from User Space's sed.c
+   Copyright (C) 2003, Lucent Technologies Inc. and others. All Rights
+   Reserved.
+  ------------------------------------------------------------------------------
+   Copyright 2006-2008 Chung, Hyung-Hwan.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+#include <qse/utl/sed.h>
+
+QSE_IMPLEMENT_COMMON_FUNCTIONS (sed)
+
+qse_sed_t* qse_sed_open (qse_mmgr_t* mmgr, qse_size_t xtn)
+{
+	return QSE_NULL;
+}
+
+void qse_sed_close (qse_sed_t* sed)
+{
+}
+
