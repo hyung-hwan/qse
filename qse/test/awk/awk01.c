231a0b18303f2eab821de17b8b07dca457e1f55f
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2009-02-17 08:00:01 +0000

    added a simple test program for awk
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@2017 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ qse/test/awk/awk01.c
@@ -0,0 +1,63 @@
+#include <qse/awk/awk.h>
+#include <qse/utl/stdio.h>
+
+const qse_char_t* src = QSE_T(
+	"BEGIN {"
+	"	for (i=2;i<=9;i++)"
+	"	{"
+	"		for (j=1;j<=9;j++)"
+	"			print i \"*\" j \"=\" i * j;"
+	"		print \"---------------------\";"
+	"	}"
+	"}"
+);
+
+int main ()
+{
+	qse_awk_t* awk = QSE_NULL;
+	qse_awk_rtx_t* rtx = QSE_NULL;
+	int ret;
+
+	awk = qse_awk_opensimple ();
+	if (awk == QSE_NULL)  
+	{
+		qse_fprintf (QSE_STDERR, QSE_T("error: cannot open awk\n"));
+		goto oops;
+	}
+
+	ret = qse_awk_parsesimple (
+		awk,
+		QSE_AWK_PARSE_STRING, src, /* parse AWK source in a string */
+		QSE_NULL /* no parse output */
+	);
+	if (ret == -1)
+	{
+		qse_fprintf (QSE_STDERR, QSE_T("error: %s\n"), 
+			qse_awk_geterrmsg(awk));
+		goto oops;
+	}
+
+	rtx = qse_awk_rtx_opensimple (
+		awk, 
+		QSE_NULL /* no console files */
+	);
+	if (rtx == QSE_NULL) 
+	{
+		qse_fprintf (QSE_STDERR, QSE_T("error: %s\n"), 
+			qse_awk_geterrmsg(awk));
+		goto oops;
+	}
+	
+	ret = qse_awk_rtx_loop (rtx);
+	if (ret == -1)
+	{
+		qse_fprintf (QSE_STDERR, QSE_T("error: %s\n"), 
+			qse_awk_rtx_geterrmsg(rtx));
+		goto oops;
+	}
+
+oops:
+	if (rtx != QSE_NULL) qse_awk_rtx_close (rtx);
+	if (awk != QSE_NULL) qse_awk_close (awk);
+	return -1;
+}
