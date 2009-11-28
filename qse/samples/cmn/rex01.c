6b98760c2c87debe752e377da6d9ba80c3fdcd35
hyung-hwan <hyunghwan.chung@gmail.com>
2009-11-28 01:01:43 +0000

    added group_t reference counting

--- qse/samples/cmn/rex01.c
+++ qse/samples/cmn/rex01.c
@@ -1,12 +1,20 @@
 #include <qse/cmn/rex.h>
 #include <qse/cmn/str.h>
 #include <qse/cmn/main.h>
+#include <qse/cmn/misc.h>
 #include <qse/cmn/stdio.h>
 
 static int rex_main (int argc, qse_char_t* argv[])
 {
 	qse_rex_t* rex;
 	qse_rex_node_t* start;
+
+	if (argc != 3)
+	{
+		qse_printf (QSE_T("USAGE: %s pattern string\n"), 
+			qse_basename(argv[0]));
+		return -1;
+	}
 
 	rex = qse_rex_open (QSE_NULL, 0, QSE_NULL);
 	if (rex == QSE_NULL)
