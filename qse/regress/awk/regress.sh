ef09923ad1cd55534d83eef39cd2da3a1b8a89c6
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2010-08-01 01:45:47 +0000

    enhanced xma realloc()
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@2288 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- qse/regress/awk/regress.sh
+++ qse/regress/awk/regress.sh
@@ -17,8 +17,8 @@ echo_title()
 	echo "--------------------------------------------------------------------------------"
 	while [ $# -gt 0 ]
 	do
-		echo -n "$1 "
-		echo -n "$1 " >/dev/stderr
+		echo -n "[CMD] $1 "
+		echo -n "[CMD] $1 " >/dev/stderr
 		shift
 	done		
 	echo
@@ -40,15 +40,23 @@ print_usage()
 	QSEAWK=../../cmd/awk/.libs/qseawk
 	[ -f "${QSEAWK}" ] || QSEAWK=../../cmd/awk/qseawk
 }
+[ -z "${QSESED}" ] && {
+	QSESED=../../cmd/sed/.libs/qsesed
+	[ -f "${QSESED}" ] || QSEAWK=../../cmd/sed/qsesed
+}
 [ -f "${QSEAWK}" -a -x "${QSEAWK}" ] || {
 	echo_so "the executable '${QSEAWK}' is not found or not executable"
+	exit 1
+}
+[ -f "${QSESED}" -a -x "${QSESED}" ] || {
+	echo_so "the executable '${QSESED}' is not found or not executable"
 	exit 1
 }
 
 TMPFILE="${TMPFILE:=./regress.temp}"
 OUTFILE="${OUTFILE:=./regress.out}"
-
-GLOBALOPTS="-m 500000"
+OUTFILE_XMA="${OUTFILE}.xma"
+XMAOPTS="-m 500000"
 
 PROGS="
 	cou-001.awk!cou.dat!!
@@ -177,6 +185,7 @@ PROGS="
 run_scripts() 
 {
 	valgrind="${1}"
+	extraopts="${2}"
 	echo "${PROGS}" > "${TMPFILE}"
 	
 	while read prog
@@ -200,50 +209,74 @@ run_scripts()
 	
 		[ -z "${redinfile}" ] && redinfile="/dev/stdin"
 
-		echo_title "${valgrind} ${QSEAWK} ${GLOBALOPTS} ${awkopts} -f ${orgscript} ${datafile} <${redinfile} 2>&1"
-		${valgrind} ${QSEAWK} ${GLOBALOPTS} -o "${script}.dp" ${awkopts} -f ${script} ${datafile} <${redinfile} 2>&1
+		echo_title "${valgrind} ${QSEAWK} ${extraopts} ${awkopts} -f ${orgscript} ${datafile} <${redinfile} 2>&1"
+		${valgrind} ${QSEAWK} ${extraopts} -o "${script}.dp" ${awkopts} -f ${script} ${datafile} <${redinfile} 2>&1
 	
 	done < "${TMPFILE}" 
 	
 	rm -f "${TMPFILE}"
 }
 
+run_test()
+{
+	outfile="${1}"
+	extraopts="${2}"
+
+	rm -f *.dp
+	echo_so "FIRST RUN WITH ORIGINAL SOURCE"
+	run_scripts "" "${extraopts}" > "${outfile}.test"
+	echo_so "SECOND RUN WITH DEPARSED SOURCE"
+	run_scripts "" "${extraopts}" > "${outfile}.test2"
+	rm -f *.dp
+
+	diff "${outfile}.test" "${outfile}.test2" > /dev/null || {
+		echo_so "ERROR: Difference is found between the first run and the second run."
+		echo_so "       The output of the first run is stored in '${outfile}.test'."
+		echo_so "       The output of the seconds run is stored in '${outfile}.test2'."
+		echo_so "       You may execute 'diff ${outfile}.test ${outfile}.test2' for more info."
+		exit 1
+	}
+
+	rm -f "${outfile}.test2"
+
+	# diff -q is not supported on old platforms.
+	# redirect output to /dev/null instead.
+	diff "${outfile}" "${outfile}.test" > /dev/null || {
+		echo_so "ERROR: Difference is found between expected output and actual output."
+		echo_so "       The expected output is stored in '${outfile}'."
+		echo_so "       The actual output is stored in '${outfile}.test'."
+		echo_so "       You may execute 'diff ${outfile} ${outfile}.test' for more info."
+		return 1
+	}
+	rm -f "${outfile}.test"
+	return 0
+}
+
 case $1 in
 init)
 	rm -f *.dp
-	run_scripts > "${OUTFILE}"
+	run_scripts "" "" > "${OUTFILE}"
+	run_scripts "" "${XMAOPTS}" > "${OUTFILE_XMA}"
 	rm -f *.dp
 	echo_so "INIT OK"
 	;;
 test)
-	rm -f *.dp
-	echo_so "FIRST RUN WITH ORIGINAL SOURCE"
-	run_scripts > "${OUTFILE}.test"
-	echo_so "SECOND RUN WITH DEPARSED SOURCE"
-	run_scripts > "${OUTFILE}.test2"
-	rm -f *.dp
-
-	diff "${OUTFILE}.test" "${OUTFILE}.test2" > /dev/null || {
-		echo_so "ERROR: Difference is found between the first run and the second run."
-		echo_so "       The output of the first run is stored in '${OUTFILE}.test'."
-		echo_so "       The output of the seconds run is stored in '${OUTFILE}.test2'."
-		echo_so "       You may execute 'diff ${OUTFILE}.test ${OUTFILE}.test2' for more info."
-		exit 1
+	run_test "${OUTFILE}" "" && {
+		run_test "${OUTFILE_XMA}" "${XMAOPTS}" && {
+			#diff "${OUTFILE}" "${OUTFILE_XMA}" | grep -v '^\[CMD\] '
+			${QSESED} "s|${QSEAWK} ${XMAOPTS}|${QSEAWK} |" "${OUTFILE_XMA}" > "${OUTFILE_XMA}.$$"
+			diff "${OUTFILE}" "${OUTFILE_XMA}.$$"  || {
+				rm -f "${OUTFILE_XMA}.$$"
+				echo_so "ERROR: Difference is found between normal output and xma output."
+				echo_so "       The normal output is stored in '${OUTFILE}'."
+				echo_so "       The xma output is stored in '${OUTFILE_XMA}'."
+				echo_so "       Ignore lines staring with [CMD] in the difference."
+				exit 1;
+			}
+			rm -f "${OUTFILE_XMA}.$$"
+			echo_so "TEST OK"
+		}
 	}
-
-	rm -f "${OUTFILE}.test2"
-
-	# diff -q is not supported on old platforms.
-	# redirect output to /dev/null instead.
-	diff "${OUTFILE}" "${OUTFILE}.test" > /dev/null || {
-		echo_so "ERROR: Difference is found between expected output and actual output."
-		echo_so "       The expected output is stored in '${OUTFILE}'."
-		echo_so "       The actual output is stored in '${OUTFILE}.test'."
-		echo_so "       You may execute 'diff ${OUTFILE} ${OUTFILE}.test' for more info."
-		exit 1
-	}
-	rm -f "${OUTFILE}.test"
-	echo_so "TEST OK"
 	;;
 leakcheck)
 	bin_valgrind="`which valgrind 2> /dev/null || echo ""`"
@@ -251,7 +284,7 @@ leakcheck)
 		echo_so "valgrind not found. cannot perform this test"
 		exit 1
 	}
-	run_scripts "${bin_valgrind} --leak-check=full --show-reachable=yes --track-fds=yes" 2>&1 > "${OUTFILE}.test"
+	run_scripts "${bin_valgrind} --leak-check=full --show-reachable=yes --track-fds=yes" "" 2>&1 > "${OUTFILE}.test"
 	x=`grep -Fic "no leaks are possible" "${OUTFILE}.test"`
 	y=`grep -Fic "${bin_valgrind}" "${OUTFILE}.test"`
 	if [ ${x} -eq ${y} ] 
@@ -261,16 +294,6 @@ leakcheck)
 		echo_so "(POSSIBLY) some memory leaks detected".
 	fi
 	echo_so "Inspect the '${OUTFILE}.test' file for details"
-	;;
-*)
-	echo_so "USAGE: $0 init"
-	echo_so "       $0 test"
-	echo_so "       $0 leakcheck"
-	exit 1
-	;;
-esac
-	
-exit 0
 	;;
 *)
 	echo_so "USAGE: $0 init"
--- qse/samples/cmn/xma.c
+++ qse/samples/cmn/xma.c
@@ -1,11 +1,15 @@
 #include <qse/cmn/xma.h>
 #include <qse/cmn/stdio.h>
 
-int main ()
+#define R(f) \
+	do { \
+		qse_printf (QSE_T("== %s ==\n"), QSE_T(#f)); \
+		if (f() == -1) return -1; \
+	} while (0)
+
+static int test1 ()
 {
-	int i;
 	void* ptr[100];
-	void* x;
 
 	qse_xma_t* xma = qse_xma_open (QSE_NULL, 0, 100000L);
 	if (xma == QSE_NULL) 
@@ -18,17 +22,88 @@ int main ()
 	ptr[1] = qse_xma_alloc (xma, 1000);
 	ptr[2] = qse_xma_alloc (xma, 3000);
 	ptr[3] = qse_xma_alloc (xma, 1000);
-	qse_xma_dump (xma);
-	qse_xma_free (xma, ptr[0]);
-	qse_xma_free (xma, ptr[2]);
+	//qse_xma_dump (xma, qse_printf);
+	//qse_xma_free (xma, ptr[0]);
+	//qse_xma_free (xma, ptr[2]);
 	//qse_xma_free (xma, ptr[3]);
 
-	qse_xma_dump (xma);
-	qse_printf (QSE_T("===============================\n"));
-	qse_xma_realloc (xma, ptr[1], 500);
-	qse_xma_dump (xma);
+	qse_xma_dump (xma, qse_printf);
+	qse_xma_realloc (xma, ptr[0], 500);
+	qse_xma_realloc (xma, ptr[3], 500);
+	qse_xma_dump (xma, qse_printf);
 
-#if 0
+	qse_xma_close (xma);
+	return 0;
+}
+
+static int test2 ()
+{
+	void* ptr[100];
+
+	qse_xma_t* xma = qse_xma_open (QSE_NULL, 0, 100000L);
+	if (xma == QSE_NULL) 
+	{
+		qse_printf (QSE_T("cannot open xma\n"));
+		return -1;
+	}
+
+	ptr[0] = qse_xma_alloc (xma, 5000);
+	ptr[1] = qse_xma_alloc (xma, 1000);
+	ptr[2] = qse_xma_alloc (xma, 3000);
+	ptr[3] = qse_xma_alloc (xma, 1000);
+	qse_xma_dump (xma, qse_printf);
+	qse_xma_free (xma, ptr[0]);
+	qse_xma_free (xma, ptr[2]);
+
+	qse_xma_dump (xma, qse_printf);
+	qse_xma_realloc (xma, ptr[1], 500);
+	qse_xma_dump (xma, qse_printf);
+
+	qse_xma_close (xma);
+	return 0;
+}
+
+static int test3 ()
+{
+	void* ptr[100];
+
+	qse_xma_t* xma = qse_xma_open (QSE_NULL, 0, 100000L);
+	if (xma == QSE_NULL) 
+	{
+		qse_printf (QSE_T("cannot open xma\n"));
+		return -1;
+	}
+
+	ptr[0] = qse_xma_alloc (xma, 5000);
+	ptr[1] = qse_xma_alloc (xma, 1000);
+	ptr[2] = qse_xma_alloc (xma, 3000);
+	ptr[3] = qse_xma_alloc (xma, 1000);
+	qse_xma_dump (xma, qse_printf);
+	qse_xma_free (xma, ptr[0]);
+	qse_xma_free (xma, ptr[2]);
+
+	qse_xma_dump (xma, qse_printf);
+	ptr[1] = qse_xma_realloc (xma, ptr[1], 3000);
+	qse_xma_dump (xma, qse_printf);
+	qse_xma_free (xma, ptr[1]);
+	qse_xma_dump (xma, qse_printf);
+
+	qse_xma_close (xma);
+	return 0;
+}
+
+static int test4 ()
+{
+	int i;
+	void* ptr[100];
+
+	qse_xma_t* xma = qse_xma_open (QSE_NULL, 0, 2000000L);
+	if (xma == QSE_NULL) 
+	{
+		qse_printf (QSE_T("cannot open xma\n"));
+		return -1;
+	}
+
 	for (i = 0; i < 100; i++)
 	{
 		int sz = (i + 1) * 10;
@@ -39,7 +114,6 @@ int main ()
 			qse_printf (QSE_T("failed to alloc %d\n"), sz);
 			break;
 		}
-		qse_printf (QSE_T("%d %p\n"), sz, ptr[i]);
 	}
 
 	for (--i; i > 0; i-= 3)
@@ -56,19 +130,27 @@ int main ()
 	{
 		void* x, * y;
 
-		qse_printf (QSE_T("%p\n"), qse_xma_alloc (xma, 5000));
-		qse_printf (QSE_T("%p\n"), qse_xma_alloc (xma, 1000));
-		qse_printf (QSE_T("%p\n"), (x = qse_xma_alloc (xma, 10)));
-		qse_printf (QSE_T("%p\n"), (y = qse_xma_alloc (xma, 40)));
+		qse_xma_alloc (xma, 5000);
+		qse_xma_alloc (xma, 1000);
+		x = qse_xma_alloc (xma, 10);
+		y = qse_xma_alloc (xma, 40);
 
 		if (x) qse_xma_free (xma, x);
 		if (y) qse_xma_free (xma, y);
-		qse_printf (QSE_T("%p\n"), (x = qse_xma_alloc (xma, 10)));
-		qse_printf (QSE_T("%p\n"), (y = qse_xma_alloc (xma, 40)));
+		x = qse_xma_alloc (xma, 10);
+		y = qse_xma_alloc (xma, 40);
 	}
-	qse_xma_dump (xma);
-#endif
+	qse_xma_dump (xma, qse_printf);
 
 	qse_xma_close (xma);
+	return 0;
+}
+
+int main ()
+{
+	R (test1);
+	R (test2);
+	R (test3);
+	R (test4);
 	return 0;
 }
