43bc5ffb7997b8b2e884ac37167cd7e40d11dec6
hyung-hwan <hyunghwan.chung@gmail.com>
2010-09-25 03:03:38 +0000

    added qse_bsearch(), qse_lsearch(), qse_qsort()

---
+++ qse/include/qse/cmn/alg.h
@@ -0,0 +1,88 @@
+/*
+ * $Id$
+ *
+    Copyright 2006-2009 Chung, Hyung-Hwan.
+    This file is part of QSE.
+
+    QSE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    QSE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with QSE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _QSE_ALG_H_
+#define _QSE_ALG_H_
+
+/** @file
+ * This file provides functions for commonly used algorithms.
+ */
+
+#include <qse/types.h>
+#include <qse/macros.h>
+
+/**
+ * The qse_search_comper_t type defines a search callback function.
+ * The callback function is called by search functions for each comparison
+ * performed. It should return 0 if @a ptr1 and @a ptr2 are 
+ * euqal, a positive integer if @a ptr1 is greater than @a ptr2, a negative 
+ * if @a ptr2 is greater than @a ptr1. Both @a ptr1 and @a ptr2 are 
+ * pointers to any two items in the array. @a ctx which is a pointer to 
+ * user-defined data passed to a search function is passed to the callback
+ * with no modification.
+ */
+typedef int (*qse_search_comper_t) (
+	const void* ptr1, 
+	const void* ptr2, 
+	void*       ctx
+);
+
+/**
+ * The qse_sort_comper_t type defines a sort callback function.
+ */
+typedef qse_search_comper_t qse_sort_comper_t;
+
+/**
+ * The qse_bsearch() function performs binary search over a sorted array.
+ */
+void* qse_bsearch (
+	const void*         key,
+	const void*         base,
+	qse_size_t          nmemb,
+     qse_size_t          size,
+	qse_search_comper_t comper,
+	void*               ctx
+);
+
+/**
+ * The qse_lsearch() function performs linear search over an array.
+ */
+void* qse_bsearch (
+	const void*         key,
+	const void*         base,
+	qse_size_t          nmemb,
+     qse_size_t          size,
+	qse_search_comper_t comper,
+	void*               ctx
+);
+
+
+/**
+ * The qse_qsort() function performs quick-sorting over an array.
+ */
+void qse_qsort (
+	void*             base,
+	qse_size_t        nmemb,
+	qse_size_t        size,
+	qse_sort_comper_t comper,
+	void*             ctx
+);
+
+#endif
