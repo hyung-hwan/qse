1c59b2a7509cb5b1226abb65dd28ff4bd417fa35
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2010-09-25 03:03:38 +0000

    added qse_bsearch(), qse_lsearch(), qse_qsort()
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@2306 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ qse/lib/cmn/alg_search.c
@@ -0,0 +1,55 @@
+/*
+ * $Id$
+ *
+    Copyright 2006-2009 Chung, Hyung-Hwan.
+    This file is part of QSE.
+
+    QSE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    QSE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with QSE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <qse/cmn/alg.h>
+
+void* qse_bsearch (
+	const void *key, const void *base, qse_size_t nmemb,
+	qse_size_t size, qse_search_comper_t comper, void* ctx)
+{
+	int n;
+	const void* mid;
+	qse_size_t lim;
+
+	for (lim = nmemb; lim > 0; lim >>= 1)
+	{
+		mid = ((qse_byte_t*)base) + (lim >> 1) * size;
+
+		n = (comper) (key, mid, ctx);
+		if (n == 0) return (void*)mid;
+		if (n > 0) { base = ((const qse_byte_t*)mid) + size; lim--; }
+	}
+
+	return QSE_NULL;
+}
+
+void* qse_lsearch (
+	const void* key, const void* base, qse_size_t nmemb,
+	qse_size_t size, qse_search_comper_t comper, void* ctx)
+{
+	while (nmemb > 0) 
+	{
+		if (comper(key, base, ctx) == 0) return (void*)base;
+		base = ((const qse_byte_t*)base) + size;
+		nmemb--;
+	}
+
+	return QSE_NULL;
+}
