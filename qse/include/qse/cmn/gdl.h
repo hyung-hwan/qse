2adaa58e7d87962f01c9d6c1690610412da663ff
hyung-hwan <hyunghwan.chung@gmail.com>
2011-06-16 10:00:36 +0000

    no meaningful changes...

--- qse/include/qse/cmn/gdl.h
+++ qse/include/qse/cmn/gdl.h
@@ -1,5 +1,21 @@
 /*
  * $Id$
+ *
+    Copyright 2006-2011 Chung, Hyung-Hwan.
+    This file is part of QSE.
+
+    QSE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    QSE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with QSE. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _QSE_CMN_GDL_H_
@@ -55,13 +71,19 @@ struct qse_gdl_t
  */
 #define QSE_GDL_TAIL(host) ((host)->prev)
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void qse_gdl_chain (qse_gdl_t* p, qse_gdl_t* x, qse_gdl_t* n);
-void qse_gdl_unchain (qse_gdl_t* x);
+void qse_gdl_chain (
+	qse_gdl_t* p,
+	qse_gdl_t* x,
+	qse_gdl_t* n
+);
+
+void qse_gdl_unchain (
+	qse_gdl_t* x
+);
 
 #ifdef __cplusplus
 }
