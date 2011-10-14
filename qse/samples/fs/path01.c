b5b23cbc3618fe7ab2cd00822120fbed119bc2c0
hyung-hwan <hyunghwan.chung@gmail.com>
2011-10-14 15:51:20 +0000

    fixed bugs in qse_canonpath()

--- qse/samples/fs/path01.c
+++ qse/samples/fs/path01.c
@@ -2,16 +2,30 @@
 #include <qse/fs/dir.h>
 #include <qse/cmn/stdio.h>
 #include <qse/cmn/main.h>
+#include <qse/cmn/str.h>
+#include <qse/cmn/mem.h>
 
 int path_main (int argc, qse_char_t* argv[])
 {
+	qse_char_t* canon;
+	qse_size_t len;
+
 	if (argc != 2)
 	{
 		qse_fprintf (QSE_STDERR, QSE_T("Usage: %s <directory>\n"), argv[0]);
 		return -1;
 	}
 
-	qse_printf (QSE_T("[%s] => [%s]\n"), argv[1], qse_canonpath (argv[1]));
+	canon = QSE_MMGR_ALLOC (QSE_MMGR_GETDFL(), (qse_strlen(argv[1]) + 1) * QSE_SIZEOF(*canon));
+	if (canon == QSE_NULL)
+	{
+		qse_fprintf (QSE_STDERR, QSE_T("Error: out of memory\n"));
+		return -1;
+	}
+
+	len = qse_canonpath (argv[1], canon);
+	qse_printf (QSE_T("[%s] => [%s] %d chars\n"), argv[1], canon, (int)len);
+	QSE_MMGR_FREE (QSE_MMGR_GETDFL(), canon);
 	return 0;
 }
 
