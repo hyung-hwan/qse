75bf46e051a168f3a23f53debd6c649020739273
hyung-hwan <hyunghwan.chung@gmail.com>
2013-04-05 01:07:45 +0000

    changed default style for directory listing and error pages

--- qse/lib/http/httpd-dir.c
+++ qse/lib/http/httpd-dir.c
@@ -592,7 +592,7 @@ qse_httpd_task_t* qse_httpd_entaskdir (
 				data.path.len = qse_mbslen(data.path.ptr);
 				data.qpath.ptr = qse_htre_getqpath(req);
 				data.qpath.len = qse_mbslen(data.qpath.ptr);
-				data.head.ptr = dir->head? dir->head: QSE_MT("");
+				data.head.ptr = dir->head? dir->head: QSE_MT("<style type='text/css'>body { background-color:#d0e4fe; font-size: 0.9em; } div.header { font-weight: bold; margin-bottom: 5px; } div.footer { border-top: 1px solid #99AABB; text-align: right; } table { font-size: 0.9em; } td { white-space: nowrap; } td.size { text-align: right; }</style>");
 				data.head.len = qse_mbslen(data.head.ptr);
 				data.foot.ptr = dir->foot? dir->foot: qse_httpd_getname(httpd);
 				data.foot.len = qse_mbslen(data.foot.ptr);
