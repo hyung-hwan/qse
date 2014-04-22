481d3e72a3592897cb665ab31fa95b2f7b00967c
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2014-04-22 15:30:28 +0000

    added some experimental code for legacy mac os
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@2968 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

--- qse/include/qse/conf-inf.h
+++ qse/include/qse/conf-inf.h
@@ -1,8 +1,8 @@
 #ifndef _QSE_CONF_INF_H_
 #define _QSE_CONF_INF_H_
 
-/* change this information whenever you update
- * package version in configure.ac */
+/* This file is used for non-autoconf based build systems.
+ * Change this information whenever you update package version in configure.ac */
 
 #define QSE_PACKAGE_VERSION "0.6.0"
 #define QSE_PACKAGE_VERSION_MAJOR 0
