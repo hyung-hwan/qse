bf063b2ae7b89c795fbc3d130595c10f8318475a
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2016-04-08 11:11:20 +0000

    added Socket.cpp
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@3257 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ qse/lib/sys/Socket.cpp
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ *
+    Copyright (c) 2006-2014 Chung, Hyung-Hwan. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <qse/sys/Socket.hpp>
+
+/////////////////////////////////
+QSE_BEGIN_NAMESPACE(QSE)
+/////////////////////////////////
+
+Socket::Socket (): handle(QSE_INVALID_SCKHND)
+{
+}
+
+Socket::~Socket ()
+{
+	this->close ();
+}
+
+
+void Socket::close ()
+{
+	if (this->handle != QSE_INVALID_SCKHND)
+	{
+		qse_closesckhnd (this->handle);
+		this->handle = QSE_INVALID_SCKHND;
+	}
+}
+
+//int Socket::open (int domain, int type, int protocol)
+//{
+//}
+
+
+int Socket::connect (const SocketAddress& target)
+{
+	if (this->handle == QSE_INVALID_SCKHND)
+	{
+		//::socket (target.getFamily(), this->type, 0);
+	}
+}
+
+int Socket::beginConnect (const SocketAddress &target)
+{
+}
+
+/////////////////////////////////
+QSE_END_NAMESPACE(QSE)
+/////////////////////////////////
