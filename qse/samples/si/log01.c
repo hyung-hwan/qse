a4aa6a8e77991084a280e58b1467632ec86baa9a
hyunghwan.chung <hyunghwan.chung@8287bb76-5ddd-404f-9b8b-d83bdfffbde0>
2017-09-16 04:10:14 +0000

    added a tester for logging functions
    git-svn-id: https://code.abiyo.net/svn/qse/trunk@3326 8287bb76-5ddd-404f-9b8b-d83bdfffbde0

---
+++ qse/samples/si/log01.c
@@ -0,0 +1,55 @@
+#include <qse/cmn/mem.h>
+#include <qse/cmn/hwad.h>
+#include <qse/si/log.h>
+#include <qse/si/nwad.h>
+#include <stdio.h>
+
+void t1 (void)
+{
+	qse_log_t* log;
+	qse_log_target_t t;
+	qse_int128_t q = 0x1234567890;
+	int i;
+	qse_nwad_t nwad;
+
+	t.file = QSE_T("/tmp/t3.log");
+	//qse_strtonwad ("127.0.0.1:560", &nwad);
+	qse_strtonwad ("@/dev/log", &nwad);
+	qse_nwadtoskad (&nwad, &t.syslog_remote);
+
+	log = qse_log_open (QSE_MMGR_GETDFL(), 0, QSE_T("t3"), 
+		QSE_LOG_INCLUDE_PID | QSE_LOG_DEBUG | QSE_LOG_CONSOLE |
+		QSE_LOG_FILE | QSE_LOG_SYSLOG | QSE_LOG_SYSLOG_REMOTE, &t);
+
+	QSE_ASSERT (qse_log_getoption (log) == QSE_LOG_INCLUDE_PID);
+	QSE_ASSERT (qse_log_gettarget (log, QSE_NULL) == (QSE_LOG_CONSOLE | QSE_LOG_FILE | QSE_LOG_SYSLOG | QSE_LOG_SYSLOG_REMOTE));
+
+	for (i = 0; i < 10; i++)
+	{
+
+		if (i == 4)
+		{
+			qse_log_target_t t2;
+
+			qse_log_gettarget (log, &t2);
+			qse_strtonwad ("127.0.0.1:560", &nwad);
+			qse_nwadtoskad (&nwad, &t2.syslog_remote);
+			qse_log_settarget (log, QSE_LOG_CONSOLE | QSE_LOG_FILE | QSE_LOG_SYSLOG_REMOTE, &t2);
+
+			qse_log_setoption (log, qse_log_getoption(log) | QSE_LOG_KEEP_FILE_OPEN);
+
+			QSE_ASSERT (qse_log_getoption (log) == (QSE_LOG_INCLUDE_PID | QSE_LOG_KEEP_FILE_OPEN));
+			QSE_ASSERT (qse_log_gettarget (log, QSE_NULL) == (QSE_LOG_CONSOLE | QSE_LOG_FILE | QSE_LOG_SYSLOG_REMOTE));
+		}
+
+		QSE_LOG4 (log, QSE_T("test"), QSE_LOG_DEBUG, QSE_T("XXXXXXXX %d %I128x %#0128I128b %l20d >>"), 10 * i , q, q, (long)45);
+	}
+
+	qse_log_close (log);
+}
+
+int main (int argc, char* argv[])
+{
+	t1 ();
+	return 0;
+}
