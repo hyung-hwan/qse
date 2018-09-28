80ceed7bb2d05df517f751a2a70218dda13c412e
hyung-hwan <hyunghwan.chung@gmail.com>
2018-09-28 16:32:36 +0000

    fixed the name of the second parameter in declaration of qse_dhcp4_find_option()

--- qse/include/qse/dhcp/dhcpmsg.h
+++ qse/include/qse/dhcp/dhcpmsg.h
@@ -29,40 +29,41 @@ enum qse_dhcp4_htype_t
 /* option codes (partial) */
 enum qse_dhcp4_opt_t
 {
-	QSE_DHCP4_OPT_PADDING        = 0x00,
-	QSE_DHCP4_OPT_SUBNET         = 0x01,
-	QSE_DHCP4_OPT_TIME_OFFSET    = 0x02,
-	QSE_DHCP4_OPT_ROUTER         = 0x03,
-	QSE_DHCP4_OPT_TIME_SERVER    = 0x04,
-	QSE_DHCP4_OPT_NAME_SERVER    = 0x05,
-	QSE_DHCP4_OPT_DNS_SERVER     = 0x06,
-	QSE_DHCP4_OPT_LOG_SERVER     = 0x07,
-	QSE_DHCP4_OPT_COOKIE_SERVER  = 0x08,
-	QSE_DHCP4_OPT_LPR_SERVER     = 0x09,
-	QSE_DHCP4_OPT_HOST_NAME      = 0x0c,
-	QSE_DHCP4_OPT_BOOT_SIZE      = 0x0d,
-	QSE_DHCP4_OPT_DOMAIN_NAME    = 0x0f,
-	QSE_DHCP4_OPT_SWAP_SERVER    = 0x10,
-	QSE_DHCP4_OPT_ROOT_PATH      = 0x11,
-	QSE_DHCP4_OPT_IP_TTL         = 0x17,
-	QSE_DHCP4_OPT_MTU            = 0x1a,
-	QSE_DHCP4_OPT_BROADCAST      = 0x1c,
-	QSE_DHCP4_OPT_NTP_SERVER     = 0x2a,
-	QSE_DHCP4_OPT_WINS_SERVER    = 0x2c,
-	QSE_DHCP4_OPT_REQUESTED_IP   = 0x32,
-	QSE_DHCP4_OPT_LEASE_TIME     = 0x33,
-	QSE_DHCP4_OPT_OVERLOAD       = 0x34, /* overload sname or file */
-	QSE_DHCP4_OPT_MESSAGE_TYPE   = 0x35,
-	QSE_DHCP4_OPT_SERVER_ID      = 0x36,
-	QSE_DHCP4_OPT_PARAM_REQ      = 0x37,
-	QSE_DHCP4_OPT_MESSAGE        = 0x38,
-	QSE_DHCP4_OPT_MAX_SIZE       = 0x39,
-	QSE_DHCP4_OPT_T1             = 0x3a,
-	QSE_DHCP4_OPT_T2             = 0x3b,
-	QSE_DHCP4_OPT_VENDOR         = 0x3c,
-	QSE_DHCP4_OPT_CLIENT_ID      = 0x3d,
-	QSE_DHCP4_OPT_RELAY          = 0x52,
-	QSE_DHCP4_OPT_END            = 0xFF
+	QSE_DHCP4_OPT_PADDING          = 0x00,
+	QSE_DHCP4_OPT_SUBNET           = 0x01,
+	QSE_DHCP4_OPT_TIME_OFFSET      = 0x02,
+	QSE_DHCP4_OPT_ROUTER           = 0x03,
+	QSE_DHCP4_OPT_TIME_SERVER      = 0x04,
+	QSE_DHCP4_OPT_NAME_SERVER      = 0x05,
+	QSE_DHCP4_OPT_DNS_SERVER       = 0x06,
+	QSE_DHCP4_OPT_LOG_SERVER       = 0x07,
+	QSE_DHCP4_OPT_COOKIE_SERVER    = 0x08,
+	QSE_DHCP4_OPT_LPR_SERVER       = 0x09,
+	QSE_DHCP4_OPT_HOST_NAME        = 0x0c,
+	QSE_DHCP4_OPT_BOOT_SIZE        = 0x0d,
+	QSE_DHCP4_OPT_DOMAIN_NAME      = 0x0f,
+	QSE_DHCP4_OPT_SWAP_SERVER      = 0x10,
+	QSE_DHCP4_OPT_ROOT_PATH        = 0x11,
+	QSE_DHCP4_OPT_IP_TTL           = 0x17,
+	QSE_DHCP4_OPT_MTU              = 0x1a,
+	QSE_DHCP4_OPT_BROADCAST        = 0x1c,
+	QSE_DHCP4_OPT_NTP_SERVER       = 0x2a,
+	QSE_DHCP4_OPT_WINS_SERVER      = 0x2c,
+	QSE_DHCP4_OPT_REQUESTED_IPADDR = 0x32,
+	QSE_DHCP4_OPT_LEASE_TIME       = 0x33,
+	QSE_DHCP4_OPT_OVERLOAD         = 0x34, /* overload sname or file */
+	QSE_DHCP4_OPT_MESSAGE_TYPE     = 0x35,
+	QSE_DHCP4_OPT_SERVER_ID        = 0x36,
+	QSE_DHCP4_OPT_PARAM_REQ        = 0x37,
+	QSE_DHCP4_OPT_MESSAGE          = 0x38,
+	QSE_DHCP4_OPT_MAX_SIZE         = 0x39,
+	QSE_DHCP4_OPT_T1               = 0x3a,
+	QSE_DHCP4_OPT_T2               = 0x3b,
+	QSE_DHCP4_OPT_VENDOR           = 0x3c,
+	QSE_DHCP4_OPT_CLIENT_ID        = 0x3d,
+	QSE_DHCP4_OPT_RELAY            = 0x52,
+	QSE_DHCP4_OPT_SUBNET_SELECTION = 0x76,
+	QSE_DHCP4_OPT_END              = 0xFF
 };
 
 /* flags for QSE_DHCP4_OPT_OVERLOAD */
@@ -193,7 +194,7 @@ QSE_EXPORT int qse_dhcp4_walk_options (
 
 QSE_EXPORT qse_dhcp4_opt_hdr_t* qse_dhcp4_find_option (
 	const qse_dhcp4_pktinf_t* pkt,
-	int                       ode
+	int                       code
 );
 
 QSE_EXPORT qse_uint8_t* qse_dhcp4_get_relay_suboption (
