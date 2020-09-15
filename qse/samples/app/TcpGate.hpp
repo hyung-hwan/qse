4d409839e0a3dc822f323637a37e98c078052c93
hyung-hwan <hyunghwan.chung@gmail.com>
2020-09-15 10:48:14 +0000

    enhanced the sample app code

--- qse/samples/app/TcpGate.hpp
+++ qse/samples/app/TcpGate.hpp
@@ -3,8 +3,53 @@
 
 #include <qse/si/TcpServer.hpp>
 #include <qse/cmn/String.hpp>
+#include <qse/cmn/HashTable.hpp>
+#include <qse/sttp/Sttp.hpp>
 
 class MainApp;
+
+class TcpGateProto: public QSE::Sttp
+{
+public:
+	typedef int (TcpGateProto::*CmdProc) (const QSE::SttpCmd& cmd);
+
+	TcpGateProto (MainApp* app, QSE::Socket* sck);
+
+	int sendCmd (const qse_mchar_t* cmd, qse_size_t nargs, ...);
+	int sendCmd (const qse_wchar_t* cmd, qse_size_t nargs, ...);
+	int sendErrCmd (ErrorNumber err);
+	int sendErrCmd (ErrorNumber err, const qse_mchar_t* msg);
+	int sendErrCmd (ErrorNumber err, const qse_wchar_t* msg);
+	int sendWarnCmd (ErrorNumber err);
+	int sendWarnCmd (ErrorNumber err, const qse_mchar_t* msg);
+	int sendWarnCmd (ErrorNumber err, const qse_wchar_t* msg);
+	int sendOkCmd ();
+	int sendOkCmd (const qse_mchar_t* msg1);
+	int sendOkCmd (const qse_mchar_t* msg1, const qse_mchar_t* msg2);
+	int sendOkCmd (const qse_wchar_t* msg1);
+	int sendOkCmd (const qse_wchar_t* msg1, const qse_wchar_t* msg2);
+
+protected:
+	MainApp* app;
+	QSE::Socket* sck;
+
+	typedef QSE::HashTable<QSE::String, CmdProc> CmdDict;
+	CmdDict cmd_dict;
+
+	int opt_autosave;
+
+	int handle_command (const QSE::SttpCmd& cmd);
+	int write_bytes (const qse_uint8_t* data, qse_size_t len);
+
+	int proc_discon (const QSE::SttpCmd& cmd);
+	int proc_shtdwn (const QSE::SttpCmd& cmd);
+	int proc_envlst (const QSE::SttpCmd& cmd);
+	int proc_envget (const QSE::SttpCmd& cmd);
+	int proc_envset (const QSE::SttpCmd& cmd);
+	int proc_envsav (const QSE::SttpCmd& cmd);
+	int proc_optget (const QSE::SttpCmd& cmd);
+	int proc_optset (const QSE::SttpCmd& cmd);
+};
 
 class TcpGate: public QSE::TcpServer, public QSE::Thread
 {
